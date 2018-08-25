c123f2ef919e42586a09f565733389b8b6f73f27
Shoaib Ahmed <2010.ahmed.shoaib@gmail.com>
2018-08-25 12:38:20 +0600

    Add files via upload

---
+++ SPOJ/Triple Fat Ladies.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pf printf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+typedef unsigned long long ll;
+using namespace std;
+
+int main()
+{
+    int tc;
+    ll n;
+    sf(tc);
+    while(tc--)
+    {
+        cin>>n;
+        if(n==1)
+            puts("192");
+        else
+            cout<<192+(n-1)*250<<endl;
+    }
+}
