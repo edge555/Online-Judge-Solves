0e07f99e5833f01c2024019240b7106f571307b3
Shoaib Ahmed <shoaib27@student.sust.edu>
2018-09-16 21:08:25 +0600

    Add files via upload

---
+++ UVA/10656 - Maximum Sum (II).cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pf printf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define all(x) (x).begin(),(x).end()
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+typedef long long int ll;
+using namespace std;
+
+int main()
+{
+    while(1)
+    {
+        int n,k;
+        sf(n);
+        if(n==0)
+            return 0;
+        queue<int>q;
+        while(n--)
+        {
+            sf(k);
+            if(k>0)
+                q.push(k);
+        }
+        if(q.empty())
+            puts("0");
+        else
+        {
+            while(!q.empty())
+            {
+                cout<<q.front();
+                if(q.size()>1)
+                    pf(" ");
+                q.pop();
+            }
+            puts("");
+        }
+    }
+}
