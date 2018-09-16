3483bc3978e9b1cbc5f1ee9089a94fb1090ca136
Shoaib Ahmed <shoaib27@student.sust.edu>
2018-09-16 06:12:58 +0600

    Add files via upload

---
+++ Codeforces/776-2-B.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pf printf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define all(x) (x).begin(),(x).end()
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+typedef long long int ll;
+using namespace std;
+#define N 100005
+bool mark[N];
+void seive()
+{
+    long long int i;
+    memset(mark,true,sizeof(mark));
+
+    for(i=4;i<N;i+=2)
+        mark[i]=false;
+
+    for(i=3;i*i<N;i++)
+    {
+        if(mark[i])
+        {
+            for(int j=i*i;j<N;j+=i*2)
+                mark[j]=false;
+        }
+    }
+}
+int main()
+{
+    seive();
+    int n,i;
+    sf(n);
+    if(n==1)
+        pf("1\n1");
+    else if(n==2)
+        pf("1\n1 1");
+    else
+    {
+        puts("2");
+        for (i=2;i<=n+1;i++)
+        {
+            if(mark[i])
+                pf("1");
+            else
+                pf("2");
+            if(i!=n+1)
+                pf(" ");
+        }
+    }
+}
