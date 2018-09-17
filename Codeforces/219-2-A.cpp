78eb62f8ab2983360779a82cef6933e708e2c50c
Shoaib Ahmed <shoaib27@student.sust.edu>
2018-09-18 00:25:10 +0600

    Add files via upload

---
+++ Codeforces/219-2-A.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pf printf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define all(x) (x).begin(),(x).end()
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+typedef long long int ll;
+using namespace std;
+
+int main()
+{
+    int k,i;
+    string a,b="";
+    sf(k);
+    cin>>a;
+    if(k==1)
+    {
+        cout<<a;
+        return 0;
+    }
+    map<char,int>mp;
+    for (i=0;i<a.size();i++)
+        mp[a[i]]++;
+    for(auto it=mp.begin();it!=mp.end();it++)
+    {
+        if(it->se!=0)
+        {
+            if(it->se%k!=0)
+            {
+                puts("-1");
+                return 0;
+            }
+            int p=it->se/k;
+            while(p--)
+                b.pb(it->fi);
+        }
+    }
+    while(k--)
+        cout<<b;
+}
