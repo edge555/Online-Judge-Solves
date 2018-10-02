57e0a46df86b59e2cd226cafdb9ac1ee7e2e72a3
Shoaib Ahmed <shoaib27@student.sust.edu>
2018-10-02 15:04:47 +0600

    Add files via upload

---
+++ UVA/13034 - Solve Everything.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pf printf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define all(x) (x).begin(),(x).end()
+#define pi pair<int,int>
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+typedef long long int ll;
+using namespace std;
+
+int main()
+{
+    int tc,j;
+    sf(tc);
+    rep(j,tc)
+    {
+        int i,k;
+        bool chk=false;
+        rep0(i,13)
+        {
+            sf(k);
+            if(k==0)
+                chk=true;
+        }
+        pf("Set #%d: ",j);
+        if(chk)
+            puts("No");
+        else
+            puts("Yes");
+    }
+}
