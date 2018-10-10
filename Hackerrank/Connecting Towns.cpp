801d764e7a62b42244e851e73583319e1c2e33c3
Shoaib Ahmed <shoaib27@student.sust.edu>
2018-10-10 16:24:26 +0600

    Add files via upload

---
+++ Hackerrank/Connecting Towns.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pf printf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1234567
+#define fi first
+#define se second
+#define all(x) (x).begin(),(x).end()
+#define pi pair<int,int>
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+typedef long long int ll;
+using namespace std;
+int main()
+{
+    int t,tc;
+    sf(tc);
+    rep(t,tc)
+    {
+        int i,n,k;
+        sf(n);
+        ll sum=1;
+        rep0(i,n-1)
+        {
+            sf(k);
+            sum=(sum%MOD)*(k%MOD);
+        }
+        cout<<sum%MOD<<endl;
+    }
+}
