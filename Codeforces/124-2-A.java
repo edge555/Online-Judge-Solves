ad9c74e791d6278af3a78b3ef3dafae5bc1198e6
Shoaib Ahmed <shoaib27@student.sust.edu>
2018-10-12 14:29:30 +0600

    Add files via upload

---
+++ Codeforces/124-2-A.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Cf {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a,b,n,i,x,y,cnt=0;
+		n=sc.nextInt();
+		a=sc.nextInt();
+		b=sc.nextInt();
+		for(i=1;i<=n;i++)
+		{
+			x=i-1;
+			y=n-i;
+			System.out.println(x+" "+y);
+			if(x>=a && y<=b) {
+				//System.out.println(i);
+				cnt++;
+			}
+		}
+		System.out.print(cnt);
+	}
+
+}
