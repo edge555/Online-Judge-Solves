870f87a710dfda660bcc9c6a7ab853445a659a8b
Shoaib Ahmed <shoaib27@student.sust.edu>
2018-10-22 04:35:23 +0600

    Add files via upload

---
+++ Codeforces/959B.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pf printf
+#define sf(num) scanf("%lld",&num)
+#define sff(num1,num2) scanf("%lld %lld",&num1,&num2)
+#define sfff(num1,num2,num3) scanf("%lld %lld %lld",&num1,&num2,&num3);
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define mem(ara) memset(ara,0,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+typedef long long int ll;
+using namespace std;
+int main()
+{
+    /*
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    */
+    ll n,k,m,i,p,j;
+    sfff(n,k,m);
+    vector<string>vec;
+    vector<ll>vec2;
+    string a,b;
+    rep0(i,n)
+    {
+        cin>>a;
+        vec.pb(a);
+    }
+    rep0(i,n)
+    {
+        sf(p);
+        vec2.pb(p);
+    }
+    map<string,ll>mp;
+    while(k--)
+    {
+        ll x,j,a,b;
+        sf(x);
+        vector<string>vec3;
+        ll mn=LONG_MAX;
+        rep0(j,x)
+        {
+            sf(a);
+            vec3.pb(vec[a-1]);
+            mn=min(vec2[a-1],mn);
+        }
+        rep0(j,vec3.size())
+            mp[vec3[j]]=mn;
+    }
+    ll sum=0;
+    while(m--)
+    {
+        cin>>a;
+        sum+=mp[a];
+    }
+    cout<<sum<<endl;
+    //cerr << "Time : " << (double)clock() / (double)CLOCKS_PER_SEC << "s\n";
+}
