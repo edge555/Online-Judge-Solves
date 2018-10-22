f86fb8002748e722f411acddf9d5a52bbf26af5b
Shoaib Ahmed <shoaib27@student.sust.edu>
2018-10-22 21:35:28 +0600

    Add files via upload

---
+++ Codeforces/807A.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pf printf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3);
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define mem(ara) memset(ara,0,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+typedef long long int ll;
+using namespace std;
+int main()
+{
+    /*
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    */
+    int n,i,a,b;
+    bool unrated=true;
+    vector<int>vec;
+    sf(n);
+    rep0(i,n)
+    {
+        sff(a,b);
+        if(a!=b)
+            unrated=false;
+        vec.pb(a);
+    }
+    if(!unrated)
+        puts("rated");
+    else
+    {
+        rep0(i,n-1)
+        {
+            if(vec[i]<vec[i+1])
+            {
+                puts("unrated");
+                return 0;
+            }
+        }
+        puts("maybe");
+    }
+    //cerr << "Time : " << (double)clock() / (double)CLOCKS_PER_SEC << "s\n";
+}
