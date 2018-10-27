1936d155c37c0191dfd703c111a75acb9ba25948
Shoaib Ahmed <shoaib27@student.sust.edu>
2018-10-28 01:14:50 +0600

    Update and rename 11150.cpp to 11150 - Cola.cpp

--- UVA/11150 - Cola.cpp
+++ UVA/11150 - Cola.cpp
@@ -15,15 +15,14 @@ int main()
     int a,n;
     while(cin>>n)
     {
-    a=n;
-    while(n>=3)
-    {
-    a=a+(n/3);
-    n=(n/3)+(n%3);
-    }
-    if(n==2)
-        a++;
-    cout<<a<<endl;
+        a=n;
+        while(n>=3)
+        {
+            a=a+(n/3);
+            n=(n/3)+(n%3);
+        }
+        if(n==2)
+            a++;
+        cout<<a<<endl;
     }
 }
-
