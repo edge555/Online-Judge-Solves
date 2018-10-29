2f59853cf737120298b8aa0dacdd4512a07a0a44
Shoaib Ahmed <shoaib27@student.sust.edu>
2018-10-29 22:59:08 +0600

    Update and rename 96-2-A.cpp to 96-A.cpp

--- Codeforces/96-A.cpp
+++ Codeforces/96-A.cpp
@@ -12,20 +12,20 @@ int main()
         if (ara[i]=='0')
         {
             if (ara[i+1]=='0' && ara[i+2]=='0' && ara[i+3]=='0' && ara[i+4]=='0' && ara[i+5]=='0' && ara[i+6]=='0')
-                {
+            {
                 cout<<"YES"<<endl;
                 cnt++;
                 break;
-                }
+            }
         }
         else if (ara[i]=='1')
         {
             if (ara[i+1]=='1' && ara[i+2]=='1' && ara[i+3]=='1' && ara[i+4]=='1' && ara[i+5]=='1' && ara[i+6]=='1')
-                {
+            {
                 cout<<"YES"<<endl;
                 cnt++;
                 break;
-                }
+            }
         }
     }
     if (cnt==0)
