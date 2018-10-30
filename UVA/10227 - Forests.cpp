bbb312902af184b68f553f8a69dc41aa455f2491
Shoaib Ahmed <shoaib27@student.sust.edu>
2018-10-31 01:03:00 +0600

    Add files via upload

---
+++ UVA/10227 - Forests.cpp
@@ -0,0 +1,65 @@
+b#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pf printf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3);
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define mem(ara) memset(ara,0,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define autto for(auto it=mp.begin();it!=mp.end();it++)
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+typedef long long int ll;
+using namespace std;
+
+int main()
+{
+
+    int t,tc;
+    scanf("%d\n",&tc);
+    rep(t,tc)
+    {
+        int n,m,i;
+        sff(n,m);
+        set<int>s[1001];
+        stringstream ss;
+        string str;
+        getchar();
+        while(1)
+        {
+            int a,b;
+            getline(cin,str);
+            if(str.size()==0)
+                break;
+            ss<<str;
+            ss>>a>>b;
+            ss.clear();
+            s[a].insert(b);
+            s[a].insert(b);
+        }
+        set<set<int>>st;
+        set<set<int>>::iterator it;
+        rep(i,n)
+        {
+                st.insert(s[i]);
+
+        }
+        cout<<st.size()<<endl;
+        if(t!=tc)
+            puts("");
+
+
+    }
+    //cerr << "Time : " << (double)clock() / (double)CLOCKS_PER_SEC << "s\n";
+}
