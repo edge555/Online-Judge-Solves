5e597a2011eabc3b23d021dd2355b51821dc20c1
Shoaib Ahmed <shoaib27@student.sust.edu>
2018-11-01 20:44:23 +0600

    Update and rename 10935.cpp to 10935 - Throwing cards away I.cpp

---
+++ UVA/10935 - Throwing cards away I.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int m,i,x;
+    queue<int>n,l;
+    while (1)
+    {
+        cin>>m;
+        if (m==0)
+        break;
+        else
+        {
+            for (i=1;i<=m;i++)
+                n.push(i);
+    
+            while (n.size()>1)
+            {
+                l.push(n.front());
+                n.pop();
+                x=n.front();
+                n.push(x);
+                n.pop();
+            }
+
+            cout<<"Discarded cards:";
+            int k=0,p=l.size();
+            while (!l.empty())
+            {
+                cout<<" "<<l.front();
+                if (k<p-1)
+                {   
+                    cout<<",";
+                    k++;
+                }
+                l.pop();
+            }
+            cout<<"\nRemaining card: "<<n.front()<<endl;
+            n.pop();
+        }
+    }
+}
