01479d724e9749dc90f7835932055a7357e6d1ae
Shoaib Ahmed <shoaib27@student.sust.edu>
2018-11-01 20:51:57 +0600

    Update and rename 11854.c to 11854 - Egypt.c

---
+++ UVA/11854 - Egypt.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    long long int a,b,c,d,e,f;
+    while(scanf("%lld %lld %lld",&a,&b,&c)==3)
+    {
+        if(a==0 && b==0 && c==0)
+            break;
+        d=a*a;
+        e=b*b;
+        f=c*c;
+        if(a!=0 || b!=0 || c!=0)
+        {
+            if(f==d+e||d==e+f||e==d+f)
+                printf("right\n");
+            else
+                printf("wrong\n");
+        }
+    }
+}
