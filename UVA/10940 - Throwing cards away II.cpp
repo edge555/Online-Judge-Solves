5f79eb31e7c0922f0169b9fa209047a470ff95d3
Shoaib Ahmed <shoaib27@student.sust.edu>
2018-11-07 17:39:14 +0600

    Add files via upload

---
+++ UVA/10940 - Throwing cards away II.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pf printf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3);
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define mem(ara) memset(ara,0,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+typedef long long int ll;
+using namespace std;
+
+int main()
+{
+    int n,i=1,m;
+    vector<int>vec;
+    while(1)
+    {
+        sf(n);
+        m=n;
+        i=1;
+        if(n==0)
+            return 0;
+        if(n==1)
+        {
+            puts("1");
+            continue;
+        }
+        while(2*i<n)
+            i*=2;
+        n%=i;
+        if(n==0)
+            n=m/2;
+        pf("%d\n",2*n);
+    }
+    //cerr<<"Time : "<<(double)clock()/(double)CLOCKS_PER_SEC<<"s\n";
+}
