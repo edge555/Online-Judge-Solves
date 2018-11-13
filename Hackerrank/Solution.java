194b07036f92b4cf1cd7ec0c5b66db0bdc786977
Shoaib Ahmed <shoaib27@student.sust.edu>
2018-11-13 12:51:22 +0600

    Add files via upload

---
+++ Hackerrank/Solution.java
@@ -0,0 +1,17 @@
+package Calc;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a=scan.nextInt();
+        double d=scan.nextDouble();
+        scan.nextLine();
+        String s=scan.nextLine();
+        System.out.println("String: "+s);
+        System.out.println("Double: "+d);
+        System.out.println("Int: "+a);
+        scan.close();
+    }
+}
