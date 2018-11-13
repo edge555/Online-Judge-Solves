c8c861aee477bf5fccf9bde394862f03654c96a3
Shoaib Ahmed <shoaib27@student.sust.edu>
2018-11-14 04:40:17 +0600

    Add files via upload

---
+++ Hackerrank/Solution.java
@@ -0,0 +1,15 @@
+
+package hackerrank;
+import java.util.*;
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(n+" x "+i+" = "+n*i);
+        }
+    }
+    
+}
