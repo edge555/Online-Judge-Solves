4bd04f380a4039e2bb9b38ff3819755b8080a541
Shoaib Ahmed <shoaib27@student.sust.edu>
2018-11-14 01:14:40 +0600

    Add files via upload

---
+++ Codeforces/Practise.java
@@ -0,0 +1,35 @@
+package practise;
+import java.util.*;
+public class Practise {
+
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt(),i,j;
+       sc.nextLine();
+       String a=sc.nextLine();
+       boolean chk=false;
+       for(i=0;i<n-1;i++)
+       {
+           if(a.charAt(i)>a.charAt(i+1))
+           {
+               chk=true;
+               break;
+               
+           }
+       }
+       if(chk==true)
+       {
+           for(j=0;j<n;j++)
+           {
+               if(j!=i)
+                   System.out.print(a.charAt(j));
+           }
+       }
+       else
+       {
+           for(j=0;j<n-1;j++)
+               System.out.print(a.charAt(j));
+       }
+    }
+    
+}
