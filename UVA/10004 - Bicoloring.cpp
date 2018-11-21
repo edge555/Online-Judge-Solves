4c8c14f9c68f983c51d3946f28cd1641478ede56
Shoaib Ahmed <shoaib27@student.sust.edu>
2018-11-21 10:37:14 +0600

    Update 10004 - Bicoloring.cpp

--- UVA/10004 - Bicoloring.cpp
+++ UVA/10004 - Bicoloring.cpp
@@ -1,71 +1,81 @@
 #include <bits/stdc++.h>
 
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL)
 #define pf printf
 #define sf(num) scanf("%d",&num)
 #define sff(num1,num2) scanf("%d %d",&num1,&num2)
-#define cspf(kk) printf("Case %d: %d\n",tc++,kk)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3);
 #define rep(i,n) for(i=1;i<=n;i++)
 #define rep0(i,n) for(i=0;i<n;i++)
 #define reps(i,a,n) for(i=a;i<=n;i++)
-typedef long long ll;
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define mem(ara) memset(ara,-1,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+typedef long long int ll;
 using namespace std;
-vector<int>adj[300];
-int col[300];
-int bfs(int n)
+int color[250],i,k;
+vector<int>vec[250];
+bool bfs()
 {
-    memset(col,-1,sizeof(col));
+    mem(color);
     queue<int>q;
-    col[n]=1;
-    q.push(n);
+    q.push(0);
+    color[0]=1;
     while(!q.empty())
     {
-       int u=q.front();
-       q.pop();
-       for (int i=0;i<adj[u].size();i++)
-       {
-           int v=adj[u][i];
-           if(col[v]==-1)
-           {
-              if(col[u]==1)
-                col[v]=2;
-              else if(col[u]==2)
-                col[v]=1;
-            q.push(v);
-           }
-           else
-           {
-              if(col[u]==col[v]){
-                return 1;
-              }
-           }
-       }
+        int p=q.front();
+        q.pop();
+        rep0(i,vec[p].size())
+        {
+            k=vec[p][i];
+            if(color[k]==-1)
+            {
+                if(color[p]==1)
+                    color[k]=2;
+                else if(color[p]==2)
+                    color[k]=1;
+                q.push(k);
+            }
+            else
+            {
+                if(color[k]==color[p])
+                    return false;
+            }
+        }
     }
-    return 2;
+    return true;
 }
-
 int main()
 {
-    while (1)
+    int n;
+    while(1)
     {
-        int n,e;
-        cin>>n;
+        sf(n);
         if(n==0)
-            break;
-        int i,j,a,b;
-        cin>>e;
-        for (i=0;i<e;i++)
+            return 0;
+        int a,b,e;
+        sf(e);
+        rep0(i,e)
         {
-            cin>>a>>b;
-            adj[a].push_back(b);
-            adj[b].push_back(a);
+            sff(a,b);
+            vec[a].pb(b);
+            vec[b].pb(a);
         }
-        int r=bfs(0);
-        if(r==1)
-            cout<<"NOT BICOLORABLE."<<endl;
+        if(bfs())
+            pf("BICOLORABLE.\n");
         else
-            cout<<"BICOLORABLE."<<endl;
-
-        for (i=0;i<300;i++)
-            adj[i].clear();
+            pf("NOT BICOLORABLE.\n");
+        rep0(i,250)
+            vec[i].clear();
     }
+    //cerr<<"Time : "<<(double)clock()/(double)CLOCKS_PER_SEC<<"s\n";
 }
