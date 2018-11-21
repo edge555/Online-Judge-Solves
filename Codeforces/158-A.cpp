dc5437bc3d28cb5e3a981f877d44fc58ff327cc4
Shoaib Ahmed <shoaib27@student.sust.edu>
2018-11-21 13:28:56 +0600

    Update and rename 158-2-A.cpp to 158-A.cpp

--- Codeforces/158-A.cpp
+++ Codeforces/158-A.cpp
@@ -6,14 +6,13 @@ int main()
     cin>>n>>p;
     int i,ara[100000];
     for (i=0;i<n;i++)
-    {
         cin>>ara[i];
-    }
+    
     int k=ara[p-1],cnt=0;
     for (i=0;i<n;i++)
     {
         if (ara[i]>=k && ara[i]>0)
             cnt++;
     }
-    cout<<cnt<<endl;
+    cout<<cnt;
 }
