13d44a0e2124fc667cdb3c4cb05d62cd6ca63863
Shoaib Ahmed <shoaib27@student.sust.edu>
2018-11-26 00:35:08 +0600

    Update 369 - Combinations.cpp

--- UVA/369 - Combinations.cpp
+++ UVA/369 - Combinations.cpp
@@ -1,18 +1,32 @@
 #include <bits/stdc++.h>
 
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL)
 #define pf printf
 #define sf(num) scanf("%d",&num)
 #define sff(num1,num2) scanf("%d %d",&num1,&num2)
-#define cspf(kk) printf("Case %d: %d\n",tc++,kk)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3);
 #define rep(i,n) for(i=1;i<=n;i++)
 #define rep0(i,n) for(i=0;i<n;i++)
 #define reps(i,a,n) for(i=a;i<=n;i++)
-typedef long long ll;
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define mem(ara) memset(ara,-1,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+typedef long long int ll;
 using namespace std;
 
 long long res[101][101];
 
-long long ncr(int a, int b){
+long long ncr(int a, int b)
+{
     if(b==0)
         return 1;
     if(a==b)
@@ -25,18 +39,18 @@ long long ncr(int a, int b){
 
     return res[a][b];
 }
-
 int main()
 {
     int n,k;
-    memset(res,-1,sizeof(res));
+    mem(res);
 
     while(1)
     {
-        cin>>n>>k;
+        sff(n,k);
         if(n==0 && k==0)
             break;
 
         cout<<n<<" things taken "<<k<<" at a time is "<<ncr(n,k)<<" exactly."<<endl;
     }
+    //cerr<<"Time : "<<(double)clock()/(double)CLOCKS_PER_SEC<<"s\n";
 }
