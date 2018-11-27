012480d2d38f2b0ea8b56c8f9805f7cadfc6a621
Shoaib Ahmed <shoaib27@student.sust.edu>
2018-11-28 04:02:21 +0600

    Add files via upload

---
+++ UVA/11650 - Mirror Clock.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pf printf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3);
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 100005
+#define mem(ara) memset(ara,0,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+typedef long long int ll;
+using namespace std;
+
+int main()
+{
+    int tc;
+    sf(tc);
+    while(tc--)
+    {
+
+        int h,m;
+        scanf("%d:%d",&h,&m);
+        int x,y;
+        x=h*60+m;
+        y=1440-x;
+        x=y/60;
+        y%=60;
+        if(x>12)
+            x-=12;
+        pf("%02d:%02d\n",x,y);
+    }
+    //cerr<<"Time : "<<(double)clock()/(double)CLOCKS_PER_SEC<<"s\n";
+}
