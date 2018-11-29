f944d4d20449029f93975105d506833fbfc33bae
Shoaib Ahmed <shoaib27@student.sust.edu>
2018-11-30 05:31:47 +0600

    Update 377-A.cpp

--- Codeforces/377-A.cpp
+++ Codeforces/377-A.cpp
@@ -1,49 +1,71 @@
 #include <bits/stdc++.h>
 
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL)
 #define pf printf
 #define sf(num) scanf("%d",&num)
 #define sff(num1,num2) scanf("%d %d",&num1,&num2)
-#define cspf(kk) printf("Case %d: %d\n",tc++,kk)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3);
 #define rep(i,n) for(i=1;i<=n;i++)
 #define rep0(i,n) for(i=0;i<n;i++)
 #define reps(i,a,n) for(i=a;i<=n;i++)
-typedef long long ll;
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define mem(ara) memset(ara,0,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define autto(vec) for(auto it=vec.begin();it!=vec.end();it++)
+#define all(x) (x).begin(),(x).end()
+#define pi pair<int,int>
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+typedef long long int ll;
 using namespace std;
-bool vis[555][555];
-char ara[555][555];
+int n,m,k;
+string a[501];
+bool vis[501][501];
 int dx[]={1,-1,0,0};
 int dy[]={0,0,1,-1};
-int row,col,k;
-void dfs(int a,int b)
+bool valid(int x,int y)
 {
-    if(a<0 || a>=row || b<0 || b>=col|| vis[a][b]==true ||ara[a][b]!='.')
-        return;
-    vis[a][b]=true;
-    for (int i=0;i<4;i++)
-        dfs(a+dx[i],b+dy[i]);
-
+    if(x<0 || x>=n || y<0 || y>=m || a[x][y]!='.' || vis[x][y] || k==0)
+        return false;
+    return true;
+}
+void dfs(int x,int y)
+{
+    int i;
+    vis[x][y]=true;
+    rep0(i,4)
+    {
+        int xx=x+dx[i];
+        int yy=y+dy[i];
+        if(valid(xx,yy))
+           dfs(xx,yy);
+    }
     if(k!=0)
     {
-        ara[a][b]='X';
+        a[x][y]='X';
         k--;
     }
 }
 int main()
 {
-    cin>>row>>col>>k;
-    getchar();
     int i,j;
-    for (i=0;i<row;i++)
-        cin>>ara[i];
-    for (i=0;i<row;i++)
+    sfff(n,m,k);
+    rep0(i,n)
+        cin>>a[i];
+
+    rep0(i,n)
     {
-        for (j=0;j<col;j++)
+        rep0(j,m)
         {
-            if(ara[i][j]=='.' && k!=0)
+            if(a[i][j]=='.' && k!=0)
                 dfs(i,j);
         }
     }
-    for (i=0;i<row;i++)
-        cout<<ara[i]<<endl;
-
+    rep0(i,n)
+        cout<<a[i]<<endl;
+    //cerr<<"Time : "<<(double)clock()/(double)CLOCKS_PER_SEC<<"s\n";
 }
