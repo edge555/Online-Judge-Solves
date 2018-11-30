986720922e3769a3449f75439173a1b091d5480c
Shoaib Ahmed <shoaib27@student.sust.edu>
2018-12-01 02:47:01 +0600

    Update 13178 - Is it multiple of 3?.cpp

--- UVA/13178 - Is it multiple of 3?.cpp
+++ UVA/13178 - Is it multiple of 3?.cpp
@@ -1,37 +1,7 @@
-#include <bits/stdc++.h>
-
-#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL)
-#define pf printf
-#define sf(num) scanf("%d",&num)
-#define sff(num1,num2) scanf("%d %d",&num1,&num2)
-#define rep(i,n) for(i=1;i<=n;i++)
-#define rep0(i,n) for(i=0;i<n;i++)
-#define reps(i,a,n) for(i=a;i<=n;i++)
-#define pb push_back
-#define mpp make_pair
-#define MOD 1000000007
-#define fi first
-#define se second
-#define all(x) (x).begin(),(x).end()
-#define pi pair<int,int>
-#define db(x) cout<<#x<<" :: "<<x<<"\n";
-#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
-typedef long long int ll;
-using namespace std;
-int main()
-{
-    int t,tc;
-    sf(tc);
-    rep(t,tc)
-    {
-        string a;
-        cin>>a;
-        ll i,sum=0;
-        rep0(i,a.size())
-            sum+=a[i]-'0';
-        if(sum%3==1)
-            puts("NO");
-        else
-            puts("YES");
-    }
-}
+n=int(input())
+for i in range (n):
+    a=input()
+    k=0;
+    for j in range (len(a)):
+        k+=int(a[j])
+    print('NO' if k%3==1 else 'YES')
