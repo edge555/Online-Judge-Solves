e7459b08a1a0cb1baed53a172dd6b0c5682bd080
Shoaib Ahmed <shoaib27@student.sust.edu>
2018-12-01 01:46:03 +0600

    Update 469 - Wetlands of Florida.cpp

--- UVA/469 - Wetlands of Florida.cpp
+++ UVA/469 - Wetlands of Florida.cpp
@@ -1,63 +1,80 @@
 #include <bits/stdc++.h>
 
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL)
 #define pf printf
-#define sf(num) scanf("%d ",&num)
+#define sf(num) scanf("%d",&num)
 #define sff(num1,num2) scanf("%d %d",&num1,&num2)
-#define cspf(kk) printf("Case %d: %d\n",tc++,kk)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3);
 #define rep(i,n) for(i=1;i<=n;i++)
 #define rep0(i,n) for(i=0;i<n;i++)
 #define reps(i,a,n) for(i=a;i<=n;i++)
-typedef long long ll;
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 105
+#define mem(ara) memset(ara,0,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+typedef long long int ll;
 using namespace std;
-bool vis[200][200];
-char ara[200][200];
+char grid[N][N];
+bool vis[N][N];
+int dx[]={-1,0,1,0,-1,1,1,-1};
+int dy[]={0,1,0,-1,1,1,-1,-1};
 int ans;
 void dfs(int x,int y)
 {
-   if(x<0 || y<0 || ara[x][y]==0|| vis[x][y]==true || ara[x][y]!='W')
+    if(x<0 || y<0 || grid[x][y]==0 || vis[x][y]==true || grid[x][y]!='W')
         return;
-   vis[x][y]=true;
-   ans++;
-   int i,j;
-   for (i=-1;i<=1;i++)
-   {
-       for (j=-1;j<=1;j++)
-       {
-          dfs(x+i,y+j);
-       }
-   }
+    int i;
+    vis[x][y]=true;
+    ans++;
+    rep0(i,8)
+    {
+        int xx=x+dx[i],yy=y+dy[i];
+        dfs(xx,yy);
+    }
 }
 int main()
 {
-   int p;
-   sf(p);
-   while(p--)
+   int tc,t=0;
+   sf(tc);
+   getchar();
+   getchar();
+   while(tc--)
    {
-      memset(ara,0,sizeof(ara));
-
+      memset(grid,0,sizeof(grid));
       char a[200];
-      int k=0;
+      int n=0;
+      if(t!=0)
+        puts("");
+      t++;
       while(gets(a))
       {
          if(a[0]=='\0')
             break;
          if(a[0]=='W' || a[0]=='L')
          {
-            sscanf(a,"%s",ara[k]);
-            k++;
+            sscanf(a,"%s",grid[n]);
+            n++;
          }
          else
          {
              int i,j;
              sscanf(a,"%d %d",&i,&j);
-             ans=0;
              memset(vis,false,sizeof(vis));
-             dfs(--i,--j);
+             i--;j--;
+             ans=0;
+             dfs(i,j);
              pf("%d\n",ans);
-
          }
       }
-      if(p!=0)
-        cout<<endl;
    }
+   //cerr<<"Time : "<<(double)clock()/(double)CLOCKS_PER_SEC<<"s\n";
 }
