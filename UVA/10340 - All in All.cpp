3e4b76dd8739462145f8d37e56c2ea919b59c8fd
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-01-08 05:55:08 +0600

    Update and rename 10340.cpp to 10340 - All in All.cpp

--- UVA/10340 - All in All.cpp
+++ UVA/10340 - All in All.cpp
@@ -11,20 +11,19 @@ int main()
 {
     string s1,s2;
     int n,cnt;
-
     while(cin>>s1>>s2)
     {
         n=s1.size();
         cnt=0;
 
         for(int i=0;i<s2.size() && cnt<n;i++)
+        {
             if(s2[i]==s1[cnt])
                 cnt++;
-
+        }
         if(cnt==n)
             cout<<"Yes"<<endl;
         else
             cout<<"No"<<endl;
     }
-
 }
