94e15bdf1b2ec79db5fcf55f55a9d31b8f88974e
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-01-08 05:46:52 +0600

    Update and rename 976-2-A.cpp to 976-A.cpp

--- Codeforces/976-A.cpp
+++ Codeforces/976-A.cpp
@@ -7,23 +7,22 @@ int main()
     cin>>n;
     getchar();
     cin>>a;
-    for (i=0;i<n;i++){
+    for (i=0;i<n;i++)
+    {
         if (a[i]=='1')
             cnt++;
         else
             cnt0++;
     }
-
     if (cnt0==n)
-        cout<<"0"<<endl;
+        cout<<"0";
     else if (cnt==n)
-        cout<<"1"<<endl;
+        cout<<"1";
     else
     {
         b.push_back('1');
-        for (i=0;i<cnt0;i++){
+        for (i=0;i<cnt0;i++)
             b.push_back('0');
-        }
-        cout<<b<<endl;
+        cout<<b;
     }
 }
