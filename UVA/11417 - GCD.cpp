de69bc1cfa3bff14ce8d264edac8b24ff47cf57b
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-01-11 19:13:12 +0600

    Add files via upload

--- UVA/11417 - GCD.cpp
+++ UVA/11417 - GCD.cpp
@@ -1,17 +1,88 @@
 #include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3);
+#define sl(num) scanf("%lld",&num)
+#define sll(num1,num2) scanf("%lld %lld",&num1,&num2)
+#define slll(num1,num2,num3) scanf("%lld %lld %lld",&num1,&num2,&num3);
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 505
+#define mem(ara) memset(ara,-1,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+typedef long long int ll;
 using namespace std;
+bool mark[N];
+void sieve()
+{
+    int i,j;
+    memset(mark,true,sizeof(mark));
+    for(i=4;i<N;i+=2)
+        mark[i]=false;
+
+    for(i=3;i*i<N;i++)
+    {
+        if(mark[i])
+        {
+            for(j=i*i;j<N;j+=i*2)
+                mark[j]=false;
+        }
+    }
+    mark[1]=false;
+}
 int main()
 {
-    while (1)
+    int n,ara[N];
+    mem(ara);
+    sieve();
+    while(1)
     {
-        int n;
-        cin>>n;
-        if (n==0)
-            break;
+        sf(n);
+        if(n==0)
+            return 0;
+        if(ara[n]!=-1)
+        {
+            pf("%d\n",ara[n]);
+            continue;
+        }
         int i,j,sum=0;
         for(i=1;i<n;i++)
+        {
             for(j=i+1;j<=n;j++)
-                sum+=__gcd(i,j);
-        cout<<sum<<endl;
+            {
+                if(mark[i])
+                {
+                    if(j%i==0)
+                        sum+=i;
+                    else
+                        sum++;
+                }
+                else
+                {
+                    if(mark[j])
+                        sum++;
+                    else
+                        sum+=__gcd(i,j);
+                }
+            }
+        }
+        pf("%d\n",sum);
+        ara[n]=sum;
     }
+    //cerr<<"Time : "<<(double)clock()/(double)CLOCKS_PER_SEC<<"s\n";
 }
