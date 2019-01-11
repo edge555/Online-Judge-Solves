f8daa95f07f03281c8315a614db4c74400fc1ff1
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-01-11 19:39:52 +0600

    Add files via upload

---
+++ UVA/11827 - Maximum GCD.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3);
+#define sl(num) scanf("%lld",&num)
+#define sll(num1,num2) scanf("%lld %lld",&num1,&num2)
+#define slll(num1,num2,num3) scanf("%lld %lld %lld",&num1,&num2,&num3);
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 100005
+#define mem(ara) memset(ara,0,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+typedef long long int ll;
+using namespace std;
+int main()
+{
+    int t,tc;
+    sf(tc);
+    getchar();
+    rep(t,tc)
+    {
+        string a;
+        getline(cin,a);
+        stringstream ss(a);
+        vector<int>vec;
+        int i,j,k,ans=-1;
+        while(ss>>k)
+            vec.pb(k);
+        rep0(i,vec.size()-1)
+        {
+            reps(j,i+1,vec.size()-1)
+                ans=max(ans,__gcd(vec[i],vec[j]));
+        }
+        pf("%d\n",ans);
+    }
+    //cerr<<"Time : "<<(double)clock()/(double)CLOCKS_PER_SEC<<"s\n";
+}
