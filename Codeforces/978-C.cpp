6695bdf4578b2651edbbaf49fd7572e693068fca
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-01-12 12:24:44 +0600

    Add files via upload

---
+++ Codeforces/978-C.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3);
+#define sl(num) scanf("%lld",&num)
+#define sll(num1,num2) scanf("%lld %lld",&num1,&num2)
+#define slll(num1,num2,num3) scanf("%lld %lld %lld",&num1,&num2,&num3);
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 100005
+#define mem(ara) memset(ara,0,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+typedef long long int ll;
+using namespace std;
+int main()
+{
+    int n,m,i;
+    sff(n,m);
+    ll k,sum=0;
+    vector<ll>vec;
+    vector<ll>::iterator it;
+    vec.pb(0);
+    rep0(i,n)
+    {
+        sl(k);
+        sum+=k;
+        vec.pb(sum);
+    }
+    while(m--)
+    {
+        sl(k);
+        it=lower_bound(all(vec),k);
+        int ind=it-vec.begin();
+        pf("%d %lld\n",ind,k-vec[ind-1]);
+    }
+    //cerr<<"Time : "<<(double)clock()/(double)CLOCKS_PER_SEC<<"s\n";
+}
