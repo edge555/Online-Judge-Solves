0aed1ed71f00a84650a9ca1efc93eb82a235018a
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-01-30 18:57:10 +0600

    Add files via upload

---
+++ UVA/10699 - Count the factors.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3);
+#define sl(num) scanf("%lld",&num)
+#define sll(num1,num2) scanf("%lld %lld",&num1,&num2)
+#define slll(num1,num2,num3) scanf("%lld %lld %lld",&num1,&num2,&num3);
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 10005
+#define mem(ara) memset(ara,0,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define vi vector<int>
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+#define TIME cerr<<"Time : "<<(double)clock()/(double)CLOCKS_PER_SEC<<"s\n";
+typedef long long int ll;
+using namespace std;
+bool mark[N];
+vector<int>prime;
+void sieve()
+{
+    int i,j;
+    memset(mark,true,sizeof(mark));
+    for(i=4;i<N;i+=2)
+        mark[i]=false;
+    for(i=3;i*i<N;i++)
+    {
+        if(mark[i])
+        {
+            for(j=i*i;j<N;j+=i*2)
+                mark[j]=false;
+        }
+    }
+    mark[1]=false;
+    prime.pb(2);
+    for(i=3;i<N;i+=2)
+    {
+        if(mark[i])
+            prime.pb(i);
+    }
+}
+int main()
+{
+    sieve();
+    int n;
+    while(1)
+    {
+        sf(n);
+        if(n==0)
+            return 0;
+        int cnt=0,m=n,i;
+        for(i=0;i<prime.size() && prime[i]*prime[i]<=n;i++)
+        {
+            if(n%prime[i]==0)
+            {
+                cnt++;
+                while(n%prime[i]==0)
+                    n/=prime[i];
+            }
+        }
+        if(n>2)
+            cnt++;
+        pf("%d : %d\n",m,cnt);
+    }
+
+}
