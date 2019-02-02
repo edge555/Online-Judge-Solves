1aab4c2bf123a6779c9c4c00a788c22daaa69d3b
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-02-02 14:43:23 +0600

    Update 11137 - Ingenuous Cubrency.cpp

--- UVA/11137 - Ingenuous Cubrency.cpp
+++ UVA/11137 - Ingenuous Cubrency.cpp
@@ -2,8 +2,13 @@
 
 #define FAST ios_base::sync_with_stdio(false); cin.tie(NULL)
 #define pf printf
+#define sc scanf
 #define sf(num) scanf("%d",&num)
 #define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3);
+#define sl(num) scanf("%lld",&num)
+#define sll(num1,num2) scanf("%lld %lld",&num1,&num2)
+#define slll(num1,num2,num3) scanf("%lld %lld %lld",&num1,&num2,&num3);
 #define rep(i,n) for(i=1;i<=n;i++)
 #define rep0(i,n) for(i=0;i<n;i++)
 #define reps(i,a,n) for(i=a;i<=n;i++)
@@ -12,30 +17,48 @@
 #define MOD 1000000007
 #define fi first
 #define se second
+#define N 10001
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
 #define all(x) (x).begin(),(x).end()
+#define vi vector<int>
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
 #define db(x) cout<<#x<<" :: "<<x<<"\n";
 #define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+#define TIME cerr<<"Time : "<<(double)clock()/(double)CLOCKS_PER_SEC<<"s\n";
 typedef long long int ll;
 using namespace std;
-
+int n,coin[22];
+ll dp[22][N];
+ll coinchange(int i,int left)
+{
+    if(i>=21)
+    {
+        if(left==0)
+            return 1;
+        else
+            return 0;
+    }
+    if(dp[i][left]!=-1)
+        return dp[i][left];
+    ll res1=0,res2=0;
+    if(left-coin[i]>=0)
+        res1=coinchange(i,left-coin[i]);
+    res2=coinchange(i+1,left);
+    return dp[i][left]=res1+res2;
+}
 int main()
 {
-    int n,i,j;
-    ll coin[25];
-    for(i=1;i<22;i++)
-        coin[i]=i*i*i;
-    ll dp[10005];
-    memset(dp,0,sizeof(dp));
-    dp[0]=1;
-    for(i=1;i<22;i++)
+    int i;
+    for(i=1;i<=21;i++)
+        coin[i-1]=i*i*i;
+    mem(dp,-1);
+    while(~sf(n))
     {
-        for(j=coin[i];j<10005;j++)
-        {
-            if(dp[j-coin[i]])
-                dp[j]+=dp[j-coin[i]];
-        }
+        ll k=coinchange(0,n);
+        pf("%lld\n",k);
     }
-    while(cin>>n)
-        cout<<dp[n]<<endl;
-
 }
