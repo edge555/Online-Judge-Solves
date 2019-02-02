fdca134246ba6975c68d27756a51cf3c4a8cec7a
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-02-02 09:14:33 +0600

    Update 674 - Coin Change.cpp

--- UVA/674 - Coin Change.cpp
+++ UVA/674 - Coin Change.cpp
@@ -2,8 +2,13 @@
 
 #define FAST ios_base::sync_with_stdio(false); cin.tie(NULL)
 #define pf printf
+#define sc scanf
 #define sf(num) scanf("%d",&num)
 #define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3);
+#define sl(num) scanf("%lld",&num)
+#define sll(num1,num2) scanf("%lld %lld",&num1,&num2)
+#define slll(num1,num2,num3) scanf("%lld %lld %lld",&num1,&num2,&num3);
 #define rep(i,n) for(i=1;i<=n;i++)
 #define rep0(i,n) for(i=0;i<n;i++)
 #define reps(i,a,n) for(i=a;i<=n;i++)
@@ -12,15 +17,23 @@
 #define MOD 1000000007
 #define fi first
 #define se second
+#define N 100005
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
 #define all(x) (x).begin(),(x).end()
+#define vi vector<int>
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
 #define db(x) cout<<#x<<" :: "<<x<<"\n";
 #define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+#define TIME cerr<<"Time : "<<(double)clock()/(double)CLOCKS_PER_SEC<<"s\n";
 typedef long long int ll;
 using namespace std;
-int n;
 int coin[]={1,5,10,25,50};
-int dp[6][7500];
-int call(int i,int amount)
+int dp[6][7500],n;
+int coinchange(int i,int amount)
 {
     if(i>=5)
     {
@@ -31,16 +44,18 @@ int call(int i,int amount)
     }
     if(dp[i][amount]!=-1)
         return dp[i][amount];
-    int ret1=0,ret2=0;
+    int res1=0,res2=0;
     if(amount-coin[i]>=0)
-        ret1=call(i,amount-coin[i]);
-    ret2=call(i+1,amount);
-    return dp[i][amount]=ret1+ret2;
-
+        res1=coinchange(i,amount-coin[i]);
+    res2=coinchange(i+1,amount);
+    return dp[i][amount]=res1+res2;
 }
 int main()
 {
-    memset(dp,-1,sizeof(dp));
-    while(cin>>n)
-        cout<<call(0,n)<<endl;
+    mem(dp,-1);
+    while(~sf(n))
+    {
+        int p=coinchange(0,n);
+        pf("%d\n",p);
+    }
 }
