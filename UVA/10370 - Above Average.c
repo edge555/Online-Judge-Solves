9ed4b2748cbef9ca0ae14073a107a5662b6446c0
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-02-02 10:38:24 +0600

    Update and rename 10370.c to 10370 - Above Average.c

--- UVA/10370 - Above Average.c
+++ UVA/10370 - Above Average.c
@@ -4,13 +4,13 @@ int main ()
     int ara[10000],i,j,n,k,cnt=0;
     float ave,pc,sum;
     scanf ("%d",&n);
-    for (i=0;i<n;i++)
+    for(i=0;i<n;i++)
     {
         scanf ("%d",&k);
         for (j=0,sum=0;j<k;j++)
         {
-        scanf ("%d",&ara[j]);
-        sum+=ara[j];
+            scanf ("%d",&ara[j]);
+            sum+=ara[j];
         }
         ave=sum/k;
         for (j=0,cnt=0;j<k;j++)
@@ -19,7 +19,7 @@ int main ()
                 cnt++;
         }
         pc=((cnt*1.0)/k)*100;
-            printf ("%0.3f%%\n",pc);
+        printf ("%0.3f%%\n",pc);
     }
-        return 0;
+    return 0;
 }
