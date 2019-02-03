fd70081fd2c520aa648f238928204991d22ea3fc
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-02-03 14:12:13 +0600

    Add files via upload

---
+++ UVA/10943 - How do you add.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3);
+#define sl(num) scanf("%lld",&num)
+#define sll(num1,num2) scanf("%lld %lld",&num1,&num2)
+#define slll(num1,num2,num3) scanf("%lld %lld %lld",&num1,&num2,&num3);
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000
+#define fi first
+#define se second
+#define N 101
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define vi vector<int>
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+#define TIME cerr<<"Time : "<<(double)clock()/(double)CLOCKS_PER_SEC<<"s\n";
+typedef long long int ll;
+using namespace std;
+ll dp[N][N];
+ll func(int n,int k)
+{
+    if(k==1)
+        return 1;
+    if(dp[n][k]!=-1)
+        return dp[n][k];
+    ll p=0,j;
+    for(j=0;j<=n;j++)
+    {
+        p+=func(n-j,k-1);
+        p%=MOD;
+    }
+    return dp[n][k]=p;
+}
+int main()
+{
+
+    while(1)
+    {
+        int n,k;
+        sff(n,k);
+        if(n+k==0)
+            return 0;
+        mem(dp,-1);
+        pf("%lld\n",func(n,k)%MOD);
+    }
+}
