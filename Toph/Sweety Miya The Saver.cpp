789dc800853ff423f88fd21b049f6068630be525
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-02-21 13:59:46 +0600

    Create Sweety Miya The Saver.cpp

---
+++ Toph/Sweety Miya The Saver.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3);
+#define sl(num) scanf("%lld",&num)
+#define sll(num1,num2) scanf("%lld %lld",&num1,&num2)
+#define slll(num1,num2,num3) scanf("%lld %lld %lld",&num1,&num2,&num3);
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 10001
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+#define TIME cerr<<"Time : "<<(double)clock()/(double)CLOCKS_PER_SEC<<"s\n";
+typedef long long int ll;
+using namespace std;
+int mx;
+string a;
+bool mark[N];
+void sieve()
+{
+    int i,j;
+    memset(mark,true,sizeof(mark));
+    for(i=4;i<N;i+=2)
+        mark[i]=false;
+    for(i=3;i*i<N;i++)
+    {
+        if(mark[i])
+        {
+            for(j=i*i;j<N;j+=i*2)
+                mark[j]=false;
+        }
+    }
+    mark[1]=false;
+}
+int getnum(string a)
+{
+    int i,p=0;
+    rep0(i,a.size())
+    {
+        p*=10;
+        p+=a[i]-'0';
+    }
+    return p;
+}
+int main()
+{
+    sieve();
+    FAST;
+    int t,tc;
+    cin>>tc;
+    rep(t,tc)
+    {
+        int n,i,j;
+        cin>>n>>a;
+        mx=-1;
+        rep0(i,n)
+        {
+            if(mx==9973)
+                break;
+            string b="";
+            reps(j,i,n-1)
+            {
+                b+=a[j];
+                if(b.size()>4)
+                    break;
+                int p=getnum(b);
+                if(mark[p])
+                    mx=max(p,mx);
+            }
+        }
+        pf("Case %d: ",t);
+        if(mx==-1)
+            puts("This is a junk!");
+        else
+            pf("%d\n",mx);
+    }
+}
