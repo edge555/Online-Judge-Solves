7e2707751b8140100771e8f83910b165a09c7060
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-02-24 01:30:48 +0600

    Added

---
+++ Codechef/The Tricky Deal.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3);
+#define sl(num) scanf("%lld",&num)
+#define sll(num1,num2) scanf("%lld %lld",&num1,&num2)
+#define slll(num1,num2,num3) scanf("%lld %lld %lld",&num1,&num2,&num3);
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 100005
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+#define TIME cerr<<"Time : "<<(double)clock()/(double)CLOCKS_PER_SEC<<"s\n";
+typedef long long int ll;
+using namespace std;
+ll a;
+ll calc(int n)
+{
+    ll x=n*a;
+    ll y=pow(2,n)-1;
+    return x-y;
+}
+int main()
+{
+    int t,tc;
+    sf(tc);
+    rep(t,tc)
+    {
+        sl(a);
+        ll d1=-1,d2,i,mx=-1;
+        rep(i,100)
+        {
+            ll ans=calc(i);
+            if(ans>0)
+            {
+                d2=i;
+                if(ans>mx)
+                {
+                    mx=ans;
+                    d1=i;
+                }
+            }
+            else
+                break;
+        }
+        pf("%lld %lld\n",d2,d1);
+    }
+}
