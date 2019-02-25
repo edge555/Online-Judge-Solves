e6e1f36e9400eb07da58bc77426c0fd8aca1102b
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-02-25 10:13:30 +0600

    Create 1130-B.cpp

---
+++ Codeforces/1130-B.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3);
+#define sl(num) scanf("%lld",&num)
+#define sll(num1,num2) scanf("%lld %lld",&num1,&num2)
+#define slll(num1,num2,num3) scanf("%lld %lld %lld",&num1,&num2,&num3);
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 100001
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+#define TIME cerr<<"Time : "<<(double)clock()/(double)CLOCKS_PER_SEC<<"s\n";
+typedef long long int ll;
+using namespace std;
+int ara[N][2];
+int main()
+{
+    int i,n,k;
+    sf(n);
+    vector<pi>vec;
+    for(i=0;i<2*n;i++)
+    {
+        sf(k);
+        vec.pb({k,i+1});
+    }
+    sort(all(vec));
+    rep0(i,vec.size())
+    {
+        pi pr=vec[i];
+        int x=pr.se,y=pr.fi;
+        if(i%2==0)
+            ara[y][0]=x;
+        else
+            ara[y][1]=x;
+    }
+    ll step=0;
+    int cur=1;
+    for(i=1;i<=n;i++)
+    {
+        step+=abs(cur-ara[i][0]);
+        cur=ara[i][0];
+    }
+    cur=1;
+    for(i=1;i<=n;i++)
+    {
+        step+=abs(cur-ara[i][1]);
+        cur=ara[i][1];
+    }
+    pf("%lld",step);
+}
