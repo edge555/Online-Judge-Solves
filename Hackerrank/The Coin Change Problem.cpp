0fe206b9dfb146a36426bf09f30b9d0a0b80eb5c
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-02-28 12:11:56 +0600

    Create The Coin Change Problem.cpp

---
+++ Hackerrank/The Coin Change Problem.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3);
+#define sl(num) scanf("%lld",&num)
+#define sll(num1,num2) scanf("%lld %lld",&num1,&num2)
+#define slll(num1,num2,num3) scanf("%lld %lld %lld",&num1,&num2,&num3);
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 100005
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+#define TIME cerr<<"Time : "<<(double)clock()/(double)CLOCKS_PER_SEC<<"s\n";
+typedef long long int ll;
+using namespace std;
+int coin[51],s,n;
+ll dp[51][251];
+ll coinchange(int ind,int sum)
+{
+    if(ind>=n)
+    {
+        if(sum==0)
+            return 1;
+        return 0;
+    }
+    if(dp[ind][sum]!=-1)
+        return dp[ind][sum];
+    ll ret1=0;
+    if(sum-coin[ind]>=0)
+        ret1=coinchange(ind,sum-coin[ind]);
+    ll ret2=coinchange(ind+1,sum);
+    return dp[ind][sum]=ret1+ret2;
+}
+int main()
+{
+
+    sff(s,n);
+    int i,k;
+    rep0(i,n)
+        sf(coin[i]);
+    mem(dp,-1);
+    pf("%lld",coinchange(0,s));
+}
