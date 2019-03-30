4fd58838764be88e6f710cc03093bcddf702a6a6
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-03-31 03:03:38 +0600

    Added

---
+++ UVA/11356 - Dates.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3);
+#define sl(num) scanf("%lld",&num)
+#define sll(num1,num2) scanf("%lld %lld",&num1,&num2)
+#define slll(num1,num2,num3) scanf("%lld %lld %lld",&num1,&num2,&num3);
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 100005
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+#define TIME cerr<<"Time : "<<(double)clock()/(double)CLOCKS_PER_SEC<<"s\n";
+typedef long long int ll;
+using namespace std;
+int intToDay(int jd)
+{
+    return jd%7;
+}
+int dateToInt(int y,int m,int d)
+{
+    return 1461* (y + 4800 + (m - 14) / 12) / 4 +
+    367 * (m - 2 - (m - 14) / 12 * 12) / 12 -
+    3 * ((y + 4900 + (m - 14) / 12) / 100) / 4 +
+    d - 32075;
+}
+void intToDate(int jd, int &y, int &m, int &d)
+{
+    int x,n,i,j;
+    x = jd + 68569;
+    n = 4 * x / 146097;
+    x -= (146097 * n + 3) / 4;
+    i = (4000 * (x + 1)) / 1461001;
+    x -= 1461 * i / 4 - 31;
+    j = 80 * x / 2447;
+    d = x - 2447 * j / 80;
+    x = j / 11;
+    m = j + 2 - 12 * x;
+    y = 100 * (n - 49) + i + x;
+}
+int main()
+{
+    int t,tc;
+    sf(tc);
+    vector<string>month={"January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November" , "December"};
+    rep(t,tc)
+    {
+        int i=5,y,m,d,k,p;
+        string s,a="";
+        cin>>s;
+        y=(s[0]-48) * 1000 + (s[1]-48) * 100 + (s[2]-48) * 10 + s[3]-48;
+        while (i < s.size() && s[i] != '-')
+            a += s[i++];
+        d = (s[i + 1] - 48) * 10 + s[i + 2] - 48;
+
+        rep0(i,12)
+        {
+            if(month[i]==a)
+            {
+                m=i+1;
+                break;
+            }
+        }
+        sf(k);
+        p=dateToInt(y,m,d);
+        p+=k;
+        intToDate(p,y,m,d);
+        pf("Case %d: %d-",t,y);
+        cout<<month[m-1];
+        pf("-%02d\n",d);
+    }
+}
---
+++ UVA/568 - Just the facts.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3);
+#define sl(num) scanf("%lld",&num)
+#define sll(num1,num2) scanf("%lld %lld",&num1,&num2)
+#define slll(num1,num2,num3) scanf("%lld %lld %lld",&num1,&num2,&num3);
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1e9+7
+#define fi first
+#define se second
+#define N 10001
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+#define TIME cerr<<"Time : "<<(double)clock()/(double)CLOCKS_PER_SEC<<"s\n";
+typedef long long int ll;
+using namespace std;
+int ara[N];
+void getfact()
+{
+    ara[0]=1;
+    ll k=1,i;
+    rep(i,N)
+    {
+        k*=i;
+        while(k%10==0)
+            k/=10;
+        ara[i]=k%10;
+        k%=10000000000;
+    }
+}
+int main()
+{
+    getfact();
+    int n,i;
+    while(~sf(n))
+    {
+        rep0(i,5-(log10(n)+1))
+            pf(" ");
+        pf("%d -> %d\n",n,ara[n]);
+    }
+}
