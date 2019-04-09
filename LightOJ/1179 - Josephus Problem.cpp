b684c401c90afe70eda2843c0ff0e1799fe38691
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-04-09 23:27:46 +0600

    Added

---
+++ LightOJ/1179 - Josephus Problem.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3);
+#define sl(num) scanf("%lld",&num)
+#define sll(num1,num2) scanf("%lld %lld",&num1,&num2)
+#define slll(num1,num2,num3) scanf("%lld %lld %lld",&num1,&num2,&num3);
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 100005
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+#define TIME cerr<<"Time : "<<(double)clock()/(double)CLOCKS_PER_SEC<<"s\n";
+typedef long long int ll;
+using namespace std;
+ll josephus(ll n,ll k,ll m)
+{
+    m=n-m;
+    if(k<=1)
+        return n-m;
+    ll i=m;
+    while(i<n)
+    {
+        ll r=(i-m+k-2)/(k-1);
+        if((i+r)>n)
+            r=n-i;
+        else if(!r)
+            r=1;
+        i+=r;
+        m=(m+(r*k))%i;
+    }
+    return m+1;
+}
+int main()
+{
+    int t,tc;
+    sf(tc);
+    rep(t,tc)
+    {
+        ll n,k;
+        sll(n,k);
+        pf("Case %d: %lld\n",t,josephus(n,k,n));
+    }
+}
