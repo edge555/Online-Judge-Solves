d04fc3b78c6186e2a57165fbd0ff525eb19d8c3d
Shoaib Ahmed <2010.ahmed.shoaib@gmail.com>
2019-05-10 14:13:10 +0600

    Added

---
+++ Toph/What's your ID.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3);
+#define sl(num) scanf("%lld",&num)
+#define sll(num1,num2) scanf("%lld %lld",&num1,&num2)
+#define slll(num1,num2,num3) scanf("%lld %lld %lld",&num1,&num2,&num3);
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 100005
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+#define TIME cerr<<"Time : "<<(double)clock()/(double)CLOCKS_PER_SEC<<"s\n";
+typedef long long int ll;
+using namespace std;
+int main()
+{
+    int tc;
+    sf(tc);
+    while(tc--)
+    {
+        int a,b,c;
+        sc("%d-%d-%d",&a,&b,&c);
+        int sem=a%10;
+        a/=10;
+        switch(b)
+        {
+            case 115:
+                pf("CSE ");
+                break;
+            case 141:
+                pf("EEE ");
+                break;
+            case 116:
+                pf("BBA ");
+                break;
+            case 117:
+                pf("LLB ");
+                break;
+            case 114:
+                pf("English ");
+                break;
+            case 111:
+                pf("Economics ");
+                break;
+        }
+        switch(sem)
+        {
+            case 1:
+                pf("Spring ");
+                break;
+            case 2:
+                pf("Summer ");
+                break;
+            case 3:
+                pf("Autumn ");
+                break;
+        }
+        pf("20");
+        pf("%02d\n",a);
+    }
+}
