69797bd2ba448cb53df301437e568cc12af4027b
Shoaib Ahmed <2010.ahmed.shoaib@gmail.com>
2019-05-14 22:44:56 +0600

    Added

---
+++ Codeforces/1165-C.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3);
+#define sl(num) scanf("%lld",&num)
+#define sll(num1,num2) scanf("%lld %lld",&num1,&num2)
+#define slll(num1,num2,num3) scanf("%lld %lld %lld",&num1,&num2,&num3);
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 100005
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+#define TIME cerr<<"Time : "<<(double)clock()/(double)CLOCKS_PER_SEC<<"s\n";
+typedef long long int ll;
+using namespace std;
+int main()
+{
+    int n,i=0,cnt=0;
+    string a,b="";
+    cin>>n>>a;
+    char prev;
+    bool chk=false;
+    while(1)
+    {
+        if(i>=n)
+            break;
+        if(!chk)
+        {
+            b+=a[i];
+            chk=true;
+            prev=a[i];
+        }
+        else
+        {
+            while(1)
+            {
+                if(i>=n)
+                    break;
+                if(a[i]==prev)
+                    i++;
+                else
+                {
+                    b+=a[i];
+                    chk=false;
+                    break;
+                }
+            }
+        }
+        i++;
+    }
+    if(b.size()&1)
+        b.pop_back();
+    cout<<n-b.size()<<endl<<b;
+}
