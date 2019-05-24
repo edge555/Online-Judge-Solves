65b41fe76618623fa336b6c8b177423c220e0c7c
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-05-24 16:52:47 +0600

    Added

---
+++ Toph/Homework.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3);
+#define sl(num) scanf("%lld",&num)
+#define sll(num1,num2) scanf("%lld %lld",&num1,&num2)
+#define slll(num1,num2,num3) scanf("%lld %lld %lld",&num1,&num2,&num3);
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 100005
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+#define TIME cerr<<"Time : "<<(double)clock()/(double)CLOCKS_PER_SEC<<"s\n";
+typedef long long int ll;
+using namespace std;
+string s;
+ll ans;
+ll getnum(string a)
+{
+    ll n=0,i;
+    rep0(i,a.size())
+    {
+        n*=10;
+        n+=a[i]-'0';
+    }
+    return n;
+}
+bool perfect(ll n)
+{
+    ll k=sqrt(n);
+    if(k*k==n)
+        return true;
+    return false;
+}
+void func(string a,int ind)
+{
+    ll now=getnum(a);
+    if(perfect(now))
+        ans=max(ans,now);
+    if(ind>=s.size())
+        return;
+    func(a,ind+1);
+    func(a+s[ind],ind+1);
+}
+int main()
+{
+    int t,tc;
+    sf(tc);
+    rep(t,tc)
+    {
+        cin>>s;
+        ans=-1;
+        func("",0);
+        if(ans==0)
+            puts("-1");
+        else
+        {
+            int dig=log10(ans)+1;
+            pf("%d\n",s.size()-dig);
+        }
+    }
+}
