8b4baae3206ef9bc1d75682e9eb301a8dd187bff
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-06-04 00:56:06 +0600

    Create 1433 - Minimum Arc Distance.cpp

---
+++ LightOJ/1433 - Minimum Arc Distance.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3);
+#define sl(num) scanf("%lld",&num)
+#define sll(num1,num2) scanf("%lld %lld",&num1,&num2)
+#define slll(num1,num2,num3) scanf("%lld %lld %lld",&num1,&num2,&num3);
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 100005
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+#define TIME cerr<<"Time : "<<(double)clock()/(double)CLOCKS_PER_SEC<<"s\n";
+typedef long long int ll;
+using namespace std;
+int main()
+{
+    int t,tc;
+    sf(tc);
+    rep(t,tc)
+    {
+        int ox,oy,ax,ay,bx,by;
+        double thita,a,b,s;
+        sff(ox,oy);
+        sff(ax,ay);
+        sff(bx,by);
+        a=sqrt(sq(ax-ox)+sq(ay-oy));
+        b=sqrt(sq(bx-ox)+sq(by-oy));
+        s=sqrt(sq(bx-ax)+sq(by-ay));
+        thita=acos((((a*a)+(b*b))-(s*s))/(2.0*a*b));
+        pf("Case %d: %f\n",t,a*thita);
+    }
+}
