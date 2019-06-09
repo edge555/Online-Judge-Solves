7b2378afe2cc8196396467df16a8747469ce066d
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-06-10 04:34:16 +0600

    Added

---
+++ UVA/640 - Self Numbers.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3);
+#define sl(num) scanf("%lld",&num)
+#define sll(num1,num2) scanf("%lld %lld",&num1,&num2)
+#define slll(num1,num2,num3) scanf("%lld %lld %lld",&num1,&num2,&num3);
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 1000000
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+#define TIME cerr<<"Time : "<<(double)clock()/(double)CLOCKS_PER_SEC<<"s\n";
+typedef long long int ll;
+using namespace std;
+bool vis[N];
+int dig(int p)
+{
+    int n=0;
+    while(p)
+    {
+        n+=p%10;
+        p/=10;
+    }
+    return n;
+}
+int main()
+{
+    queue<int>q;
+    int i;
+    rep(i,N)
+    {
+        if(!vis[i])
+        {
+            q.push(i);
+            while(!q.empty())
+            {
+                int p=q.front();
+                q.pop();
+                int x=p+dig(p);
+                if(x>N)
+                    continue;
+                if(!vis[x])
+                {
+                    vis[x]=true;
+                    q.push(x);
+                }
+            }
+        }
+    }
+    rep(i,N)
+    {
+        if(!vis[i])
+            pf("%d\n",i);
+    }
+
+}
