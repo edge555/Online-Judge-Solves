72f2a6738449ac77f5c76ccad074b24c557a11b8
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-06-17 15:29:14 +0600

    Added

---
+++ SPOJ/TWENDS - Two Ends.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3);
+#define sl(num) scanf("%lld",&num)
+#define sll(num1,num2) scanf("%lld %lld",&num1,&num2)
+#define slll(num1,num2,num3) scanf("%lld %lld %lld",&num1,&num2,&num3);
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 1005
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+#define TIME cerr<<"Time : "<<(double)clock()/(double)CLOCKS_PER_SEC<<"s\n";
+typedef long long int ll;
+using namespace std;
+vector<int>vec;
+int dp[N][N];
+int func(int i,int j)
+{
+    if(i>j)
+        return 0;
+    if(dp[i][j]!=-1)
+        return dp[i][j];
+    int ans1=0,ans2=0;
+
+    if(vec[i+1]<vec[j])
+        ans1+=func(i+1,j-1)+vec[i];
+    else
+        ans1+=func(i+2,j)+vec[i];
+
+    if(vec[i]<vec[j-1])
+        ans2+=func(i,j-2)+vec[j];
+    else
+        ans2+=func(i+1,j-1)+vec[j];
+
+    return dp[i][j]=max(ans1,ans2);
+}
+int main()
+{
+    int n,tc=1;
+    while(1)
+    {
+        sf(n);
+        if(n==0)
+            return 0;
+        int i,k,sum=0;
+        rep0(i,n)
+        {
+            sf(k);
+            sum+=k;
+            vec.pb(k);
+        }
+        mem(dp,-1);
+        int res=func(0,n-1);
+        pf("In game %d, the greedy strategy might lose by as many as %d points.\n",tc++,2*res-sum);
+        vec.clear();
+    }
+}
