6f689d8a7aadb2738d7eef99eb36c74f34563147
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-06-29 23:38:27 +0600

    Added

---
+++ Codechef/Magda and Silly Pairs.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3);
+#define sl(num) scanf("%lld",&num)
+#define sll(num1,num2) scanf("%lld %lld",&num1,&num2)
+#define slll(num1,num2,num3) scanf("%lld %lld %lld",&num1,&num2,&num3);
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 100005
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+#define TIME cerr<<"Time : "<<(double)clock()/(double)CLOCKS_PER_SEC<<"s\n";
+typedef long long int ll;
+using namespace std;
+int main()
+{
+    int t,tc;
+    sf(tc);
+    rep(t,tc)
+    {
+        int n,i;
+        sf(n);
+        ll k,sum=0;
+        int evenf=0,oddf=0,evens=0,odds=0;
+        rep0(i,n)
+        {
+            sl(k);
+            sum+=k;
+            if(k&1)
+                oddf++;
+            else
+                evenf++;
+        }
+        rep0(i,n)
+        {
+            sl(k);
+            sum+=k;
+            if(k&1)
+                odds++;
+            else
+                evens++;
+        }
+        ll cnt=0;
+        if(evenf!=evens)
+            cnt+=max(evens,evenf)-min(evens,evenf);
+        if(oddf!=odds)
+            cnt+=max(odds,oddf)-min(odds,oddf);
+        cnt/=2;
+        pf("%lld\n",(sum-cnt)>>1);
+    }
+}
