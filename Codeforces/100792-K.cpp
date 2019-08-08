de2b3f99975b841f1ae145db661d86cd4cd169f6
Shoaib Ahmed <2010.ahmed.shoaib@gmail.com>
2019-08-09 01:02:00 +0600

    Added

---
+++ Codeforces/100792-K.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3)
+#define sl(num) scanf("%lld",&num)
+#define sll(num1,num2) scanf("%lld %lld",&num1,&num2)
+#define slll(num1,num2,num3) scanf("%lld %lld %lld",&num1,&num2,&num3)
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 200005
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+#define TIME cerr<<"Time : "<<(double)clock()/(double)CLOCKS_PER_SEC<<"s\n";
+typedef long long int ll;
+using namespace std;
+int n,m;
+stack<int>s;
+vector<int>grid[N];
+unordered_map<int,int>indeg;
+priority_queue<int>pq;
+void topsort()
+{
+    int i;
+    rep(i,n)
+    {
+        if(!indeg[i])
+            pq.push(i);
+    }
+    while(!pq.empty())
+    {
+        int u=pq.top();
+        pq.pop();
+        s.push(u);
+        int i,v;
+        rep0(i,grid[u].size())
+        {
+            v=grid[u][i];
+            indeg[v]--;
+            if(!indeg[v])
+                pq.push(v);
+        }
+    }
+}
+int main()
+{
+    sff(n,m);
+    int i,a,b;
+    rep0(i,m)
+    {
+        sff(a,b);
+        grid[b].pb(a);
+        indeg[a]++;
+    }
+    topsort();
+    while(!s.empty())
+    {
+        pf("%d ",s.top());
+        s.pop();
+    }
+}
