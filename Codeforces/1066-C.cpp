6bcf49cd9ba681880a224cfb7fc6823c4824ad40
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-08-15 04:59:01 +0600

    Added

---
+++ Codeforces/1066-C.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3)
+#define sl(num) scanf("%lld",&num)
+#define sll(num1,num2) scanf("%lld %lld",&num1,&num2)
+#define slll(num1,num2,num3) scanf("%lld %lld %lld",&num1,&num2,&num3)
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 200005
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+#define TIME cerr<<"Time : "<<(double)clock()/(double)CLOCKS_PER_SEC<<"s\n";
+typedef long long int ll;
+using namespace std;
+int flag[N],ara[N];
+int main()
+{
+    FAST;
+    int q,l=0,r=0,k;
+    cin>>q;
+    char ch;
+    while(q--)
+    {
+        cin>>ch>>k;
+        if(ch=='L')
+        {
+            ara[k]=++l;
+            flag[k]=0;
+        }
+        else if(ch=='R')
+        {
+            ara[k]=++r;
+            flag[k]=1;
+        }
+        else
+        {
+            if(!flag[k])
+                cout<<min(l-ara[k],r+ara[k]-1)<<endl;
+            else
+                cout<<min(r-ara[k],l+ara[k]-1)<<endl;
+        }
+    }
+}
