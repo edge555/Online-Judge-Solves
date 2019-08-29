df8036cca2672cf073556ae07754fea71b10416c
Shoaib Ahmed <2010.ahmed.shoaib@gmail.com>
2019-08-30 03:26:23 +0600

    Added

---
+++ UVA/256 - Quirksome Squares.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(num) scanf("%d",&num)
+#define sff(num1,num2) scanf("%d %d",&num1,&num2)
+#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3)
+#define sl(num) scanf("%lld",&num)
+#define sll(num1,num2) scanf("%lld %lld",&num1,&num2)
+#define slll(num1,num2,num3) scanf("%lld %lld %lld",&num1,&num2,&num3)
+#define rep(i,n) for(i=1;i<=n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+#define mpp make_pair
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 100005
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define sqr(x) ((x)*(x))
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+#define TIME cerr<<"Time : "<<(double)clock()/(double)CLOCKS_PER_SEC<<"s\n";
+typedef long long int ll;
+using namespace std;
+int main()
+{
+    vector<int>sq;
+    int i,n;
+    rep0(i,10000)
+        sq.pb(i*i);
+    while(~sf(n))
+    {
+        int x,y;
+        rep0(i,sq.size())
+        {
+            if(sq[i]>=pow(10,n))
+                break;
+            int m=pow(10,n/2);
+            x=sq[i]/m;
+            y=sq[i]%m;
+            if(sqr(x+y)==sq[i])
+                cout<<setfill('0')<<setw(n)<<sq[i]<<endl;
+        }
+    }
+}
