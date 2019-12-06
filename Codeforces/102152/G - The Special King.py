193a0370442bbd6c5d11f51f87735a010092b9be
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-12-06 15:59:39 +0600

    Added and Moved

---
+++ Codeforces/102152/G - The Special King.py
@@ -0,0 +1,3 @@
+for _ in range(int(input())):
+    a,b,c,d=map(int,input().split())
+    print(abs(c-a)+abs(d-b))
---
+++ Codeforces/1265/C. Beautiful Regional Contest.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(n) scanf("%d",&n)
+#define sff(n1,n2) scanf("%d %d",&n1,&n2)
+#define sfff(n1,n2,n3) scanf("%d %d %d",&n1,&n2,&n3)
+#define sl(n) scanf("%lld",&n)
+#define sll(n1,n2) scanf("%lld %lld",&n1,&n2)
+#define slll(n1,n2,n3) scanf("%lld %lld %lld",&n1,&n2,&n3)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define rep(i,n) for(i=1;i<=n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define nl puts("");
+#define pb push_back
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 100005
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define sz(x) x.size()
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define line puts("-------");
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+typedef long long int ll;
+using namespace std;
+int main()
+{
+    int t,tc;
+    sf(tc);
+    rep(t,tc)
+    {
+        int i,n,k;
+        vector<int>vec;
+        sf(n);
+        rep0(i,n)
+        {
+            sf(k);
+            vec.pb(k);
+        }
+        sort(all(vec));
+        multiset<pi>st;
+        i=0;
+        int cnt,sum=0;
+        while(i<n)
+        {
+            k=vec[i];
+            cnt=0;
+            while(vec[i]==k && i<n)
+            {
+                i++;
+                cnt++;
+            }
+            sum+=cnt;
+            st.insert({k,cnt});
+        }
+        while(sum>n/2)
+        {
+            auto it=st.begin();
+            sum-=it->se;
+            st.erase(it);
+        }
+        if(sz(st)<3)
+        {
+            puts("0 0 0");
+            continue;
+        }
+        auto it=st.rbegin();
+        int f=it->se;
+        sum-=f;
+        it++;
+        k=0;
+        while(k<=f)
+        {
+            k+=it->se;
+            it++;
+        }
+        int s=k;
+        sum-=k;
+        if(f<s && f<sum)
+            cout<<f<<" "<<s<<" "<<sum<<endl;
+        else
+            puts("0 0 0");
+    }
+}
