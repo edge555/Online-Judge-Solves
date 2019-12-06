58eeecf2d5dec8764382e4b26357e28179e2c554
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-12-07 04:39:04 +0600

    Added

---
+++ Toph/Meena o Rajur Boring Class.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(n) scanf("%d",&n)
+#define sff(n1,n2) scanf("%d %d",&n1,&n2)
+#define sfff(n1,n2,n3) scanf("%d %d %d",&n1,&n2,&n3)
+#define sl(n) scanf("%lld",&n)
+#define sll(n1,n2) scanf("%lld %lld",&n1,&n2)
+#define slll(n1,n2,n3) scanf("%lld %lld %lld",&n1,&n2,&n3)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define rep(i,n) for(i=1;i<=n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define nl puts("");
+#define pb push_back
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 100005
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define sz(x) x.size()
+#define PI 3.141593
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define line puts("-------");
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+typedef long long int ll;
+using namespace std;
+int main()
+{
+    double fib[50];
+    fib[0]=0.0;
+    fib[1]=1.0;
+    int i;
+    for(i=2;i<=50;i++)
+        fib[i]=fib[i-1]+fib[i-2];
+    int t,tc;
+    cin>>tc;
+    rep(t,tc)
+    {
+        double n,ans=0.0;
+        cin>>n;
+        for(i=1;i<=n;i++)
+            ans+=(fib[i]*fib[i])-((PI*fib[i]*fib[i])/4.0);
+        cout<<fixed<<setprecision(2)<<ans<<endl;
+    }
+}
