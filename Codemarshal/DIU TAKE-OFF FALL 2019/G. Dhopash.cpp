9e1b7fa14ed6dc275189e6d80f627eba7f486d30
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-12-21 02:53:18 +0600

    Added and Renamed

---
+++ Codemarshal/DIU TAKE-OFF FALL 2019/G. Dhopash.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(n) scanf("%d",&n)
+#define sff(n1,n2) scanf("%d %d",&n1,&n2)
+#define sfff(n1,n2,n3) scanf("%d %d %d",&n1,&n2,&n3)
+#define sl(n) scanf("%lld",&n)
+#define sll(n1,n2) scanf("%lld %lld",&n1,&n2)
+#define slll(n1,n2,n3) scanf("%lld %lld %lld",&n1,&n2,&n3)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define rep(i,n) for(i=1;i<=n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define nl puts("");
+#define pb push_back
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 100005
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define sz(x) x.size()
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define line puts("-------");
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+typedef long long int ll;
+using namespace std;
+int main()
+{
+    int t,tc;
+    cin>>tc;
+    rep(t,tc)
+    {
+        int i,n,k;
+        unordered_map<string,int>mp;
+        string a;
+        cin>>n;
+        rep0(i,4)
+        {
+            cin>>a>>k;
+            mp[a]=k;
+        }
+        int cnt=0;
+        while(mp["Arms"]>1 && mp["Backs"]>1 && mp["Seats"]>1 && mp["Structure"]>0)
+        {
+            cnt++;
+            mp["Arms"]-=2;
+            mp["Backs"]-=2;
+            mp["Seats"]-=2;
+            mp["Structure"]--;
+        }
+        cout<<"Case "<<t<<": ";
+        cnt*2>=n?cout<<"Carry On!":cout<<"Abort! Abort!";
+        cout<<endl<<endl;
+    }
+}
