3df448403b33395b07972e17cf93b4824726e7bb
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-12-22 12:58:32 +0600

    Added

---
+++ Codeforces/1263/D. Secret Passwords.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(n) scanf("%d",&n)
+#define sff(n1,n2) scanf("%d %d",&n1,&n2)
+#define sfff(n1,n2,n3) scanf("%d %d %d",&n1,&n2,&n3)
+#define sl(n) scanf("%lld",&n)
+#define sll(n1,n2) scanf("%lld %lld",&n1,&n2)
+#define slll(n1,n2,n3) scanf("%lld %lld %lld",&n1,&n2,&n3)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define rep(i,n) for(i=1;i<=n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define nl puts("");
+#define pb push_back
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 200005
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define sz(x) x.size()
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define line puts("-------");
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+typedef long long int ll;
+using namespace std;
+vector<int>grid[27];
+bool vis[27],ara[27];
+void dfs(int u)
+{
+    vis[u]=true;
+    int i,v;
+    rep0(i,sz(grid[u]))
+    {
+        v=grid[u][i];
+        if(!vis[v])
+            dfs(v);
+    }
+}
+int main()
+{
+    FAST;
+    int i,j,n;
+    cin>>n;
+    string a;
+    rep0(i,n)
+    {
+        cin>>a;
+        char ch=a[0];
+        int c=ch-'a';
+        ara[c]=true;
+        rep(j,sz(a)-1)
+        {
+            int k=a[j]-'a';
+            ara[k]=true;
+            grid[k].pb(c);
+            grid[c].pb(k);
+        }
+    }
+    int color=0;
+    rep0(i,26)
+    {
+        if(!ara[i])
+            continue;
+        if(!vis[i])
+        {
+            color++;
+            dfs(i);
+        }
+    }
+    cout<<color;
+}
