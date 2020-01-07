21d0be14f509ce4cfca55981b92024c44081fb4e
Shoaib Ahmed <shoaib27@student.sust.edu>
2020-01-07 17:36:04 +0600

    Added

---
+++ Codeforces/584/D. Dima and Lisa.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(n) scanf("%d",&n)
+#define sff(n1,n2) scanf("%d %d",&n1,&n2)
+#define sfff(n1,n2,n3) scanf("%d %d %d",&n1,&n2,&n3)
+#define sl(n) scanf("%lld",&n)
+#define sll(n1,n2) scanf("%lld %lld",&n1,&n2)
+#define slll(n1,n2,n3) scanf("%lld %lld %lld",&n1,&n2,&n3)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define rep(i,n) for(i=1;i<=n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define nl puts("");
+#define pb push_back
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 1000005
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define sz(x) x.size()
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define line puts("-------");
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+typedef long long int ll;
+using namespace std;
+vector<int>primes;
+bool mark[N];
+void sieve()
+{
+    int i,j;
+    memset(mark,true,sizeof(mark));
+    for(i=4;i<N;i+=2)
+        mark[i]=false;
+    for(i=3;i*i<N;i+=2)
+    {
+        if(mark[i])
+        {
+            for(j=i*i;j<N;j+=i*2)
+                mark[j]=false;
+        }
+    }
+    mark[1]=false;
+    primes.pb(2);
+    for(i=3;i<=N;i+=2)
+    {
+        if(mark[i])
+            primes.pb(i);
+    }
+}
+bool prime(int n)
+{
+    if(n==1)
+        return false;
+    if(n==2)
+        return true;
+    int i;
+    for(i=0;sq(primes[i])<=n;i++)
+    {
+        if(n%primes[i]==0)
+            return false;
+    }
+    return true;
+}
+int main()
+{
+    sieve();
+    int i,n;
+    sf(n);
+    if(prime(n))
+    {
+        pf("1\n%d",n);
+        return 0;
+    }
+    if(prime(n-2))
+    {
+        pf("2\n2 %d",n-2);
+        return 0;
+    }
+    if(prime(n-3))
+    {
+        pf("2\n3 %d",n-3);
+        return 0;
+    }
+    pf("3\n3 ");
+    n-=3;
+    for(i=1;i<=n/2;i++)
+    {
+        if(prime(i) && prime(n-i))
+        {
+            pf("%d %d",i,n-i);
+            return 0;
+        }
+    }
+}
