ab2a2a00a4c9d4e25f0e6f6981f029ba9f4221ee
Shoaib Ahmed <shoaib27@student.sust.edu>
2020-01-18 11:13:10 +0600

    Added

---
+++ LightOJ/1311 - Unlucky Bird.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(n) scanf("%d",&n)
+#define sff(n1,n2) scanf("%d %d",&n1,&n2)
+#define sfff(n1,n2,n3) scanf("%d %d %d",&n1,&n2,&n3)
+#define sl(n) scanf("%lld",&n)
+#define sll(n1,n2) scanf("%lld %lld",&n1,&n2)
+#define slll(n1,n2,n3) scanf("%lld %lld %lld",&n1,&n2,&n3)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define rep(i,n) for(i=1;i<=n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define nl puts("");
+#define pb push_back
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 100005
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define sz(x) x.size()
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define line puts("-------");
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+typedef long long int ll;
+using namespace std;
+int main()
+{
+    int tt,tc;
+    cin>>tc;
+    rep(tt,tc)
+    {
+        double v1,v2,v3,a1,a2,t,t1,t2,d1,d2,res;
+        cin>>v1>>v2>>v3>>a1>>a2;
+        t1=v1/a1;
+        t2=v2/a2;
+        t=t1;
+        if(t2>t1)
+            t=t2;
+        d1=(v1*v1)/(2*a1);
+        d2=(v2*v2)/(2*a2);
+        res=t*v3;
+        cout<<"Case "<<tt<<": "<<fixed<<setprecision(10)<<d1+d2<<" "<<res<<endl;
+    }
+}
