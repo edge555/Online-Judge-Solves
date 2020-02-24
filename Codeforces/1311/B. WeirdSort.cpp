9bca54beee80da6b04c29bf062a2879860e13a69
Shoaib Ahmed <shoaib27@student.sust.edu>
2020-02-24 22:54:40 +0600

    Added

---
+++ Codeforces/1311/B. WeirdSort.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(n) scanf("%d",&n)
+#define sff(n1,n2) scanf("%d %d",&n1,&n2)
+#define sfff(n1,n2,n3) scanf("%d %d %d",&n1,&n2,&n3)
+#define sl(n) scanf("%lld",&n)
+#define sll(n1,n2) scanf("%lld %lld",&n1,&n2)
+#define slll(n1,n2,n3) scanf("%lld %lld %lld",&n1,&n2,&n3)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define rep(i,n) for(i=1;i<=n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define nl puts("");
+#define pb push_back
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 100005
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define sz(x) x.size()
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define line puts("-------");
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+typedef long long int ll;
+using namespace std;
+int main()
+{
+    int t,tc;
+    sf(tc);
+    rep(t,tc)
+    {
+        int n,m,i,k;
+        cin>>n>>m;
+        vector<int>vec;
+        int ind[N];
+        memb(ind);
+        rep0(i,n)
+        {
+            cin>>k;
+            vec.pb(k);
+        }
+        rep0(i,m)
+        {
+            cin>>k;
+            ind[k-1]=true;
+        }
+        bool ans=true;
+        int chk=0;
+        while(1)
+        {
+            int cnt=1;
+            rep0(i,n-1)
+            {
+                if(vec[i]<=vec[i+1])
+                    cnt++;
+                else
+                    break;
+            }
+            if(cnt==n)
+            {
+                ans=true;
+                break;
+            }
+            while(vec[i]>vec[i+1] && i<n-1)
+            {
+                if(!ind[i]){
+                    ans=false;
+                    break;
+                }
+                else
+                    swap(vec[i],vec[i+1]);
+                i++;
+            }
+            if(!ans)
+                break;
+        }
+        int cnt=1;
+        rep0(i,n-1)
+        {
+            if(vec[i]<=vec[i+1])
+                cnt++;
+            else
+                break;
+        }
+        if(cnt==n)
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+    }
+}
