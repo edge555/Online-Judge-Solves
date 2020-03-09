888f2e06eb377a7d25b86b06b4cfd81d7c9c68c4
Shoaib Ahmed <shoaib27@student.sust.edu>
2020-03-09 23:55:52 +0600

    Added

---
+++ Codeforces/1312/C. Adding Powers.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(n) scanf("%d",&n)
+#define sff(n1,n2) scanf("%d %d",&n1,&n2)
+#define sfff(n1,n2,n3) scanf("%d %d %d",&n1,&n2,&n3)
+#define sl(n) scanf("%lld",&n)
+#define sll(n1,n2) scanf("%lld %lld",&n1,&n2)
+#define slll(n1,n2,n3) scanf("%lld %lld %lld",&n1,&n2,&n3)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define rep(i,n) for(i=1;i<=n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define nl puts("");
+#define pb push_back
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 100005
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define sz(x) x.size()
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define line puts("-------");
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+typedef long long int ll;
+using namespace std;
+int main()
+{
+    int t,tc;
+    sf(tc);
+    rep(t,tc)
+    {
+        ll i,n,k,p;
+        sll(n,k);
+        unordered_map<ll,bool>mp;
+        vector<ll>vec;
+        rep0(i,n)
+        {
+            sl(p);
+            vec.pb(p);
+        }
+        bool f=true;
+        rep0(i,n)
+        {
+            if(vec[i]==0)
+                continue;
+            ll now=vec[i],cur=0;
+            while(now)
+            {
+                if(now%k>1)
+                {
+                    f=false;
+                    break;
+                }
+                if(now%k==1)
+                {
+                    if(mp[cur])
+                    {
+                        f=false;
+                        break;
+                    }
+                    mp[cur]=true;
+                }
+                now/=k;
+                cur++;
+            }
+            if(!f)
+                break;
+        }
+        puts(f?"YES":"NO");
+    }
+}
