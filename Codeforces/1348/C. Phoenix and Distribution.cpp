3422e39be7effbbdc8de2748fb753f387df33c99
Shoaib Ahmed <shoaib27@student.sust.edu>
2020-05-02 00:09:38 +0600

    Added

---
+++ Codeforces/1348/C. Phoenix and Distribution.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(n) scanf("%d",&n)
+#define sff(n1,n2) scanf("%d %d",&n1,&n2)
+#define sfff(n1,n2,n3) scanf("%d %d %d",&n1,&n2,&n3)
+#define sl(n) scanf("%lld",&n)
+#define sll(n1,n2) scanf("%lld %lld",&n1,&n2)
+#define slll(n1,n2,n3) scanf("%lld %lld %lld",&n1,&n2,&n3)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define rep(i,n) for(i=1;i<=n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define nl puts("");
+#define pb push_back
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 100005
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define sz(x) x.size()
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define line puts("-------");
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+typedef long long int ll;
+using namespace std;
+
+int main()
+{
+    FAST;
+    int t,tc;
+    cin>>tc;
+    rep(t,tc)
+    {
+        int i,n,k;
+        string s;
+        cin>>n>>k>>s;
+        sort(all(s));
+        vector<string>a(k),b(k);
+        rep0(i,k)
+        {
+            a[i].clear();
+            b[i].clear();
+        }
+        if(k==1)
+        {
+            cout<<s<<endl;
+            continue;
+        }
+        if(k==n)
+        {
+            cout<<s[n-1]<<endl;
+            continue;
+        }
+        rep0(i,k)
+        {
+            a[i]+=s[i];
+            b[i]+=s[i];
+        }
+        reps(i,k,n-1)
+        {
+            a[0]+=s[i];
+            b[i%k]+=s[i];
+        }
+        sort(all(a));
+        sort(all(b));
+        cout<<min(a[k-1],b[k-1])<<endl;
+    }
+}
