50796387ee8eb6937ee8510f5670ea09cb90034d
Shoaib Ahmed <shoaib27@student.sust.edu>
2020-05-11 18:30:16 +0600

    Added

---
+++ UVA/12893 - Count It.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(n) scanf("%d",&n)
+#define sff(n1,n2) scanf("%d %d",&n1,&n2)
+#define sfff(n1,n2,n3) scanf("%d %d %d",&n1,&n2,&n3)
+#define sl(n) scanf("%lld",&n)
+#define sll(n1,n2) scanf("%lld %lld",&n1,&n2)
+#define slll(n1,n2,n3) scanf("%lld %lld %lld",&n1,&n2,&n3)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define rep(i,n) for(i=1;i<=n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define nl puts("");
+#define pb push_back
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 1000005
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define sz(x) x.size()
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define line puts("-------");
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+typedef long long int ll;
+using namespace std;
+ll ara[N];
+ll func(ll n)
+{
+    if(n==0)
+        return 0;
+    return func(n/2)+(n%2);
+}
+int main()
+{
+    int i;
+    ara[0]=0;
+    rep(i,N-1)
+        ara[i]=ara[i/2]+i%2;
+    int t,tc;
+    sf(tc);
+    ll k;
+    rep(t,tc)
+    {
+        sl(k);
+        if(k<N)
+            pf("%lld\n",ara[k]);
+        else
+            pf("%lld\n",func(k));
+    }
+
+}
