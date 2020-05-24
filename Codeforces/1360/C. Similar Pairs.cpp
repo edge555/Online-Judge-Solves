20661ab685cd41f6fd58f64d0ba6e669dc6d7701
Shoaib Ahmed <2010.ahmed.shoaib@gmail.com>
2020-05-24 21:35:02 +0600

    Added

---
+++ Codeforces/1360/C. Similar Pairs.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+#define sf(n) scanf("%d",&n)
+#define sff(n1,n2) scanf("%d %d",&n1,&n2)
+#define sfff(n1,n2,n3) scanf("%d %d %d",&n1,&n2,&n3)
+#define sl(n) scanf("%lld",&n)
+#define sll(n1,n2) scanf("%lld %lld",&n1,&n2)
+#define slll(n1,n2,n3) scanf("%lld %lld %lld",&n1,&n2,&n3)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define rep(i,n) for(i=1;i<=n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define nl puts("");
+#define pb push_back
+#define MOD 1000000007
+#define fi first
+#define se second
+#define N 100005
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define sz(x) x.size()
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+#define line puts("-------");
+#define db(x) cout<<#x<<" :: "<<x<<"\n";
+#define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define fr freopen("input.txt","r",stdin);
+#define fw freopen("output.txt","w",stdout);
+typedef long long int ll;
+using namespace std;
+int main()
+{
+    int t,tc;
+    sf(tc);
+    rep(t,tc)
+    {
+        int i,n,k,even=0;
+        vector<int>vec;
+        sf(n);
+        rep0(i,n)
+        {
+            sf(k);
+            if(k%2==0)
+                even++;
+            vec.pb(k);
+        }
+        int odd=n-even;
+        if(even%2==0 && odd%2==0)
+        {
+            puts("YES");
+            continue;
+        }
+        sort(all(vec));
+        int cnt=0;
+        if(even%2!=0 && odd%2!=0)
+        {
+            bool f=false;
+            rep0(i,n-1)
+            {
+                if(vec[i+1]-vec[i]==1)
+                {
+                    f=true;
+                    break;
+                }
+            }
+            if(!f)
+                puts("NO");
+            else
+                puts("YES");
+        }
+        else
+            puts("NO");
+
+    }
+}
