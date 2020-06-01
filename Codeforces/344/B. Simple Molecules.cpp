bc0657e50c20eba9be1e750b5fa2d47d3b021f8c
Shoaib Ahmed <2010.ahmed.shoaib@gmail.com>
2020-06-01 20:29:04 +0600

    Added

---
+++ Codeforces/344/B. Simple Molecules.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+
+#define sf(n) scanf("%d",&n)
+#define sff(n1,n2) scanf("%d %d",&n1,&n2)
+#define sfff(n1,n2,n3) scanf("%d %d %d",&n1,&n2,&n3)
+#define sl(n) scanf("%lld",&n)
+#define sll(n1,n2) scanf("%lld %lld",&n1,&n2)
+#define slll(n1,n2,n3) scanf("%lld %lld %lld",&n1,&n2,&n3)
+
+#define rep0(i,n) for(i=0;i<n;i++)
+#define rep(i,n) for(i=1;i<=n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+#define pb push_back
+
+#define fi first
+#define se second
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define sz(x) x.size()
+
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+
+#define endl "\n";
+#define line puts("-------");
+#define dbb(x) cout<<#x<<" : "<<x<<"\n";
+
+const int N = 100005;
+const int MOD = 1000000007;
+
+using ll = long long;
+using namespace std;
+
+void __print(int x) {cerr << x;} void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;} void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;} void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;} void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;} void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';} void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V> void __print(const pair<T, V> &x)
+{cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T> void __print(const T &x)
+{int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V> void _print(T t, V... v)
+{__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifndef ONLINE_JUDGE
+    #define db(x...) cerr << "[" << #x << "] = [", _print(x)
+#endif
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        //freopen("output.txt","w",stdout);
+    #endif
+    int a,b,c;
+    sfff(a,b,c);
+    int i,x,y,z;
+    rep0(i,1e6)
+    {
+        x=i;
+        z=a-i;
+        if(z<0)
+            continue;
+        y=b-x;
+        if(y<0)
+            continue;
+        if(x+z==a && x+y==b && y+z==c)
+        {
+            pf("%d %d %d",x,y,z);
+            return 0;
+        }
+    }
+    puts("Impossible");
+}
