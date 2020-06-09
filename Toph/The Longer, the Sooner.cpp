d61fe043acac0d5c4ab0bf0c3e77e5295a4f17a6
Shoaib Ahmed <2010.ahmed.shoaib@gmail.com>
2020-06-09 23:25:42 +0600

    Added

---
+++ Toph/The Longer, the Sooner.cpp
@@ -0,0 +1,122 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+
+#define sf(n) scanf("%d",&n)
+#define sff(n1,n2) scanf("%d %d",&n1,&n2)
+#define sfff(n1,n2,n3) scanf("%d %d %d",&n1,&n2,&n3)
+#define sl(n) scanf("%lld",&n)
+#define sll(n1,n2) scanf("%lld %lld",&n1,&n2)
+#define slll(n1,n2,n3) scanf("%lld %lld %lld",&n1,&n2,&n3)
+
+#define rep0(i,n) for(i=0;i<n;i++)
+#define rep(i,n) for(i=1;i<=n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define sz(x) x.size()
+
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+
+#define endl "\n";
+#define line puts("-------");
+#define dbb(x) cout<<#x<<" : "<<x<<"\n";
+
+const int N = 1005;
+const int MOD = 1000000007;
+
+void __print(int x) {cerr << x;} void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;} void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;} void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;} void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;} void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';} void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V> void __print(const pair<T, V> &x)
+{cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T> void __print(const queue<T> &x)
+{queue<T>temp=x;cerr<<"\n-----\n";while(!temp.empty())
+{cerr << temp.front() << endl;temp.pop();}cerr<<"-----\n";}
+template<typename T> void __print(const stack<T> &x)
+{stack<T>temp=x;cerr<<"\n-----\n";while(!temp.empty())
+{cerr << temp.top() << endl;temp.pop();}cerr<<"-----\n";}
+template<typename T> void __print(const T &x)
+{int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V> void _print(T t, V... v)
+{__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifndef ONLINE_JUDGE
+    #define db(x...) cerr << "[" << #x << "] = [", _print(x)
+#endif
+using ll = long long;
+vector<int>vec[N];
+vector<int>::iterator it;
+bool vis[N];
+void reset()
+{
+    int i;
+    rep0(i,N-1)
+    {
+        vec[i].clear();
+        vis[i]=false;
+    }
+}
+int main()
+{
+    #ifndef ONLINE_JUDGE
+        //freopen("input.txt", "r", stdin);
+        //freopen("output.txt","w",stdout);
+    #endif
+    int t,tc;
+    sf(tc);
+    rep(t,tc)
+    {
+        reset();
+        int i,n,k;
+        sf(n);
+        rep0(i,n)
+        {
+            sf(k);
+            vec[k].pb(i);
+        }
+        int ans=1,cnt,last=-1;
+        for(i=0;i<=1000;i++)
+        {
+            if(vec[i].empty() || vis[i])
+                continue;
+            cnt=0;
+            int cur=i;
+            while(1)
+            {
+                it=upper_bound(all(vec[cur]),last);
+                if(it==vec[cur].end())
+                {
+                    last=-1;
+                    ans=max(ans,cnt);
+                    break;
+                }
+                else
+                {
+                    vis[i]=true;
+                    cnt++;
+                    last=*it;
+                    cur++;
+                }
+            }
+        }
+        pf("Case #%d: %d\n",t,ans);
+    }
+}
