0c85079edc587bbfb908d19df6f9c81a2b29791e
Shoaib Ahmed <2010.ahmed.shoaib@gmail.com>
2020-07-23 03:25:41 +0600

    Removed

--- Codeforces/515/A. Drazil and Date.cpp
+++ Codeforces/515/A. Drazil and Date.cpp
@@ -2,13 +2,8 @@
 
 #define FAST ios_base::sync_with_stdio(false); cin.tie(NULL)
 #define pf printf
-#define sc scanf
 #define sf(num) scanf("%d",&num)
 #define sff(num1,num2) scanf("%d %d",&num1,&num2)
-#define sfff(num1,num2,num3) scanf("%d %d %d",&num1,&num2,&num3);
-#define sl(num) scanf("%lld",&num)
-#define sll(num1,num2) scanf("%lld %lld",&num1,&num2)
-#define slll(num1,num2,num3) scanf("%lld %lld %lld",&num1,&num2,&num3);
 #define rep(i,n) for(i=1;i<=n;i++)
 #define rep0(i,n) for(i=0;i<n;i++)
 #define reps(i,a,n) for(i=a;i<=n;i++)
@@ -17,31 +12,34 @@
 #define MOD 1000000007
 #define fi first
 #define se second
-#define N 100005
-#define mem(ara) memset(ara,0,sizeof(ara))
-#define memb(ara) memset(ara,false,sizeof(ara))
 #define all(x) (x).begin(),(x).end()
 #define pi pair<int,int>
-#define pii pair<pair<int,int>,pair<int,int> >
 #define db(x) cout<<#x<<" :: "<<x<<"\n";
 #define dbb(x,y) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
 typedef long long int ll;
 using namespace std;
+
 int main()
 {
-    int t,tc;
-    sf(tc);
-    rep(t,tc)
+    int a,b,x,y,cnt=0,k;
+    sff(a,b);
+    sf(k);
+    if(a<0)
+        a*=-1;
+    if(b<0)
+        b*=-1;
+    x=min(a,b);
+    cnt+=(x*2);
+    cnt+=(a-x);
+    cnt+=(b-x);
+    k-=cnt;
+    if(k<0)
+        pf("No");
+    else
     {
-        int n,p;
-        sf(n);
-        int k=1;
-        while(n--)
-        {
-            sf(p);
-            int x=p/(__gcd(k,p));
-            k=x*k;
-        }
-        pf("%d\n",k);
+        if(k%2==0)
+            pf("Yes");
+        else
+            pf("No");
     }
 }
