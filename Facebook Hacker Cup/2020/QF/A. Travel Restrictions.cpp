9bbf8d39029c93274d7a793f4498bf04f5f32b3e
Shoaib Ahmed <2010.ahmed.shoaib@gmail.com>
2020-07-28 10:05:13 +0600

    Added

---
+++ Facebook Hacker Cup/2020/QF/A. Travel Restrictions.cpp
@@ -0,0 +1,133 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+
+#define sf(n) scanf("%d",&n)
+#define sff(n1,n2) scanf("%d %d",&n1,&n2)
+#define sfff(n1,n2,n3) scanf("%d %d %d",&n1,&n2,&n3)
+#define sl(n) scanf("%lld",&n)
+#define sll(n1,n2) scanf("%lld %lld",&n1,&n2)
+#define slll(n1,n2,n3) scanf("%lld %lld %lld",&n1,&n2,&n3)
+
+#define rep0(i,n) for(i=0;i<n;i++)
+#define rep(i,n) for(i=1;i<=n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define sz(x) x.size()
+
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+
+#define nl puts("");
+#define line puts("-------");
+#define dbb(x) cout<<#x<<" : "<<x<<"\n";
+
+void __print(int x) {cerr << x;} void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;} void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;} void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;} void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;} void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';} void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V> void __print(const pair<T, V> &x)
+{cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T> void __print(const queue<T> &x)
+{queue<T>temp=x;cerr<<"\n-----\n";while(!temp.empty())
+{cerr << temp.front() << endl;temp.pop();}cerr<<"-----\n";}
+template<typename T> void __print(const stack<T> &x)
+{stack<T>temp=x;cerr<<"\n-----\n";while(!temp.empty())
+{cerr << temp.top() << endl;temp.pop();}cerr<<"-----\n";}
+template<typename T> void __print(const T &x)
+{int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V> void _print(T t, V... v)
+{__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifndef ONLINE_JUDGE
+    #define db(x...) cerr << "[" << #x << "] = [", _print(x)
+#endif
+using ll = long long;
+const int N = 51;
+const int MOD = 1000000007;
+int n,ara[N][N];
+bool path[N][N];
+void floyd()
+{
+    int i,j,k;
+    rep0(i,n)
+    {
+        rep0(j,n)
+        {
+            rep0(k,n)
+            {
+                if(ara[i][k]==1 && ara[k][j]==1)
+                    ara[i][j]=1;
+            }
+        }
+    }
+}
+int main()
+{
+    int t,tc;
+    cin>>tc;
+    rep(t,tc)
+    {
+        mem(ara,-1);
+        memb(path);
+        int i,j,k;
+        string in,out;
+        cin>>n>>in>>out;
+        rep0(i,n)
+        {
+            if(in[i]=='N')
+            {
+                for(j=0;j<n;j++)
+                    ara[j][i]=0;
+            }
+        }
+        rep0(i,n)
+        {
+            if(out[i]=='N')
+            {
+                for(j=0;j<n;j++)
+                    ara[i][j]=0;
+            }
+        }
+        int st=0;
+        rep0(i,n)
+        {
+            rep0(j,n)
+            {
+                if(abs(i-j)==1 && ara[i][j]!=0)
+                    ara[i][j]=1;
+            }
+        }
+        floyd();
+        pf("Case #%d: \n",t);
+        rep0(i,n)
+        {
+            rep0(j,n)
+            {
+                if(i==j)
+                    pf("Y");
+                else if(ara[i][j]==1)
+                    pf("Y");
+                else
+                    pf("N");
+            }
+            puts("");
+        }
+    }
+}
