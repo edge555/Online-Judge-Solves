7c9e9cf12598410702a316575fcc146ca730de50
Shoaib Ahmed <shoaib27@student.sust.edu>
2020-09-25 04:46:56 +0600

    Added

---
+++ Codeforces/1075/B. Taxi drivers and Lyft.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+
+#define sf(n) scanf("%d",&n)
+#define sff(n1,n2) scanf("%d %d",&n1,&n2)
+#define sfff(n1,n2,n3) scanf("%d %d %d",&n1,&n2,&n3)
+#define sl(n) scanf("%lld",&n)
+#define sll(n1,n2) scanf("%lld %lld",&n1,&n2)
+#define slll(n1,n2,n3) scanf("%lld %lld %lld",&n1,&n2,&n3)
+
+#define rep0(i,n) for(i=0;i<n;i++)
+#define rep(i,n) for(i=1;i<=n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define sz(x) x.size()
+
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+
+#define endl "\n";
+#define line puts("-------");
+#define dbb(x) cout<<#x<<" : "<<x<<"\n";
+
+void __print(int x) {cerr << x;} void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;} void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;} void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;} void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;} void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';} void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V> void __print(const pair<T, V> &x)
+{cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T> void __print(const queue<T> &x)
+{queue<T>temp=x;cerr<<"\n-----\n";while(!temp.empty())
+{cerr << temp.front() << endl;temp.pop();}cerr<<"-----\n";}
+template<typename T> void __print(const stack<T> &x)
+{stack<T>temp=x;cerr<<"\n-----\n";while(!temp.empty())
+{cerr << temp.top() << endl;temp.pop();}cerr<<"-----\n";}
+template<typename T> void __print(const T &x)
+{int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V> void _print(T t, V... v)
+{__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifndef ONLINE_JUDGE
+    #define db(x...) cerr << "[" << #x << "] = [", _print(x)
+#endif
+
+const int N = 100005;
+const int MOD = 1000000007;
+using ll = long long;
+int ara[2*N],ara2[2*N],ans[2*N];
+int main()
+{
+    #ifndef ONLINE_JUDGE
+        //freopen("input.txt", "r", stdin);
+        //freopen("output.txt","w",stdout);
+    #endif
+    int i,n,m,k;
+    sff(n,m);
+    vector<int>vec,vec2;
+    rep0(i,n+m)
+    {
+        sf(k);
+        vec.pb(k);
+    }
+    rep0(i,n+m)
+    {
+        sf(k);
+        vec2.pb(k);
+    }
+    int last=-1;
+    rep0(i,n+m)
+    {
+        if(vec2[i]==1)
+            last=i;
+        ara[i]=last;
+    }
+    last=-1;
+    for(i=n+m-1;i>=0;i--)
+    {
+        if(vec2[i]==1)
+            last=i;
+        ara2[i]=last;
+    }
+    rep0(i,n+m)
+    {
+        if(vec2[i]!=1)
+        {
+            int x=2e9,y=2e9;
+            if(ara[i]!=-1)
+                x=abs(vec[i]-vec[ara[i]]);
+            if(ara2[i]!=-1)
+                y=abs(vec[i]-vec[ara2[i]]);
+            if(y<x)
+                ans[ara2[i]]++;
+            else
+                ans[ara[i]]++;
+        }
+    }
+    rep0(i,n+m)
+    {
+        if(vec2[i]==1)
+            pf("%d ",ans[i]);
+    }
+}
