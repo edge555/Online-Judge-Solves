257ca9301d958c7b339eec4004694a51c619d345
Shoaib Ahmed <2010.ahmed.shoaib@gmail.com>
2021-05-10 00:11:25 +0600

    Replay of Intra AUST Programming Contest, Spring 2020

---
+++ Toph/Beautiful String.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+
+#define sf(n) scanf("%d",&n)
+#define sff(n1,n2) scanf("%d %d",&n1,&n2)
+#define sfff(n1,n2,n3) scanf("%d %d %d",&n1,&n2,&n3)
+#define sl(n) scanf("%lld",&n)
+#define sll(n1,n2) scanf("%lld %lld",&n1,&n2)
+#define slll(n1,n2,n3) scanf("%lld %lld %lld",&n1,&n2,&n3)
+
+#define rep0(i,n) for(i=0;i<n;i++)
+#define rep(i,n) for(i=1;i<=n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define sz(x) x.size()
+
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+
+#define pi pair<int,int>
+#define pii pair<pair<int,int>,pair<int,int> >
+
+#define endl "\n";
+#define line puts("-------");
+#define dbb(x) cout<<#x<<" : "<<x<<"\n";
+
+void __print(int x) {cerr << x;} void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;} void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;} void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;} void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;} void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';} void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V> void __print(const pair<T, V> &x)
+{cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T> void __print(const queue<T> &x)
+{queue<T>temp=x;cerr<<"\n-----\n";while(!temp.empty())
+{cerr << temp.front() << endl;temp.pop();}cerr<<"-----\n";}
+template<typename T> void __print(const stack<T> &x)
+{stack<T>temp=x;cerr<<"\n-----\n";while(!temp.empty())
+{cerr << temp.top() << endl;temp.pop();}cerr<<"-----\n";}
+template<typename T> void __print(const T &x)
+{int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V> void _print(T t, V... v)
+{__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifndef ONLINE_JUDGE
+    #define db(x...) cerr << "[" << #x << "] = [", _print(x)
+#endif
+
+const int N = 100005;
+const int MOD = 999961;
+using ll = long long;
+ll lex(string a)
+{
+    ll i,now=0;
+    rep0(i,sz(a))
+    {
+        ll p=a[i]-'a'+1;
+        now=((now%MOD)*(26%MOD))%MOD;
+        now=((now%MOD)+(p%MOD))%MOD;
+    }
+    return now;
+}
+int main()
+{
+    #ifndef ONLINE_JUDGE
+        //freopen("input.txt", "r", stdin);
+        //freopen("output.txt","w",stdout);
+    #endif
+    int a,b,c;
+    sfff(a,b,c);
+    vector<ll>vec,vec2,vec3;
+    int i;
+    string s;
+    rep0(i,a)
+    {
+        cin>>s;
+        vec.pb(lex(s));
+    }
+    rep0(i,b)
+    {
+        cin>>s;
+        vec2.pb(lex(s));
+    }
+    rep0(i,c)
+    {
+        cin>>s;
+        vec3.pb(lex(s));
+    }
+    sort(all(vec));
+    sort(all(vec2));
+    sort(all(vec3));
+    ll ans=0;
+    rep0(i,b)
+    {
+        ll now=vec2[i];
+        ll ind1=lower_bound(all(vec),now)-vec.begin();
+        ll ind2=lower_bound(all(vec3),now)-vec3.begin();
+        ll temp=((ind1%MOD)*(ind2%MOD))%MOD;
+        ans=((ans%MOD)+(temp%MOD))%MOD;
+    }
+    cout<<ans;
+}
