21d282e69a779178f76f8e4ac5a8a8dcf772590b
Shoaib Ahmed <2010.ahmed.shoaib@gmail.com>
2021-05-09 23:57:17 +0600

    Added

---
+++ LeetCode/Problems/58. Length of Last Word.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        stringstream ss(s);
+        string t="";
+        while(ss>>t){
+
+        }
+        return t.size();
+    }
+};
