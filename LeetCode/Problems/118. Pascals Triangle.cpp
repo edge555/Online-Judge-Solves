68af6962ede7f9087e391a31c481efd862076684
Shoaib Ahmed <2010.ahmed.shoaib@gmail.com>
2021-08-04 08:20:01 +0600

    Added

---
+++ LeetCode/Problems/118. Pascals Triangle.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int> >ans;
+        vector<int>vec={1};
+        ans.push_back(vec);
+        int i,j;
+        for(i=1;i<numRows;i++)
+        {
+            vec.clear();
+            vec={1};
+            for(j=1;j<i;j++)
+            {
+                vec.push_back(ans[i-1][j-1]+ans[i-1][j]);
+            }
+            vec.push_back(1);
+            ans.push_back(vec);
+        }
+        return ans;
+    }
+};
