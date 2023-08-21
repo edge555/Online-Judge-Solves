452a85ea69d89a3691a04b5dfb7d95d1996b736d
Shoaib Ahmed <2010.ahmed.shoaib@gmail.com>
2023-08-21 13:22:33 +0600

    added

---
+++ Codeforces/1849/A. Morning Sandwich.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+
+#define sf(n) scanf("%d",&n)
+#define sff(n1,n2) scanf("%d %d",&n1,&n2)
+#define sfff(n1,n2,n3) scanf("%d %d %d",&n1,&n2,&n3)
+#define sl(n) scanf("%lld",&n)
+#define sll(n1,n2) scanf("%lld %lld",&n1,&n2)
+#define slll(n1,n2,n3) scanf("%lld %lld %lld",&n1,&n2,&n3)
+
+#define rep0(i,n) for(i=0;i<n;i++)
+#define rep(i,n) for(i=1;i<=n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define sz(x) x.size()
+
+const int N = 100005;
+const int MOD = 1000000007;
+using ll = long long;
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+        //freopen("input.txt", "r", stdin);
+        //freopen("output.txt","w",stdout);
+    #endif
+    int t,tc;
+    sf(tc);
+    rep(t,tc)
+    {
+        int b,c,h;
+        sfff(b,c,h);
+        int r=c+h;
+        if(b>=r+1){
+            pf("%d\n",(r*2)+1);
+        }
+        else{
+            pf("%d\n",((b-1)*2)+1);
+        }
+    }
+}
+
---
+++ Codeforces/1857/A. Array Coloring.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pf printf
+#define sc scanf
+
+#define sf(n) scanf("%d",&n)
+#define sff(n1,n2) scanf("%d %d",&n1,&n2)
+#define sfff(n1,n2,n3) scanf("%d %d %d",&n1,&n2,&n3)
+#define sl(n) scanf("%lld",&n)
+#define sll(n1,n2) scanf("%lld %lld",&n1,&n2)
+#define slll(n1,n2,n3) scanf("%lld %lld %lld",&n1,&n2,&n3)
+
+#define rep0(i,n) for(i=0;i<n;i++)
+#define rep(i,n) for(i=1;i<=n;i++)
+#define reps(i,a,n) for(i=a;i<=n;i++)
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+#define sq(x) ((x)*(x))
+#define sz(x) x.size()
+
+#define mem(ara,n) memset(ara,n,sizeof(ara))
+#define memb(ara) memset(ara,false,sizeof(ara))
+
+const int N = 100005;
+const int MOD = 1000000007;
+using ll = long long;
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+        //freopen("input.txt", "r", stdin);
+        //freopen("output.txt","w",stdout);
+    #endif
+    int t,tc;
+    sf(tc);
+    rep(t,tc)
+    {
+        int i,n,k,sum=0;
+        sf(n);
+        rep0(i,n){
+            sf(k);
+            sum+=k;
+        }
+        puts(sum&1?"NO":"YES");
+    }
+}
+
